10408790dcc3c4848643ccf1e0a2e6635da1aa70
KuzyaDomovoi <vasilyshtanko@gmail.com>
2023-08-24 12:30:53 +0300

    C

--- lesson3.c
+++ lesson3.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
-int main(void);
+int main(void)
 {
-    
+    long long var_lli = 0;
+
+    int res = scanf("%lld", &var_lli);
+
+    printf("res = %d: var_lli = %lld\n", res, var_lli);
+
     return 0;
 }
