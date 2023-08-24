a6c80df5faba3d8582b73a13bb0a9bb182e7cd0b
KuzyaDomovoi <vasilyshtanko@gmail.com>
2023-08-24 11:28:17 +0300

    C

--- lesson2.c
+++ lesson2.c
@@ -2,6 +2,11 @@
 
 int main(void)
 {
-    
+    char byte1 = '0', byte2 = '0';
+
+    int res = scanf("%c %c", &byte1, &byte2);
+
+    printf("res = %d, byte1 = %c, byte2 = %c", res, byte1, byte2);
+
     return 0;
 }
