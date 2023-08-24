9702d38e32de6171d8f4f85598adda5ade30c386
KuzyaDomovoi <vasilyshtanko@gmail.com>
2023-08-24 12:20:54 +0300

    C

--- lesson2.c
+++ lesson2.c
@@ -4,9 +4,9 @@ int main(void)
 {
     char byte1 = '0', byte2 = '0';
 
-    int res = scanf("%c %c", &byte1, &byte2);
+    int count = scanf("%c %c", &byte1, &byte2);
 
-    printf("res = %d, byte1 = %c, byte2 = %c\n", res, byte1, byte2);
+    printf("count = %d, byte1 = %c, byte2 = %c\n", count, byte1, byte2);
 
     return 0;
 }
