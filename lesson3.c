cf54e9e2c0614abcd67c2e47d968349db93fcfbb
KuzyaDomovoi <vasilyshtanko@gmail.com>
2023-08-24 12:44:56 +0300

    C

--- lesson3.c
+++ lesson3.c
@@ -3,10 +3,12 @@
 int main(void)
 {
     long long var_lli = 0;
+    double var_d = 0;
+    float var_e = 0;
 
-    int res = scanf("%lld", &var_lli);
+    int res = scanf("%lld %lf %f", &var_lli, &var_d, &var_e);
 
-    printf("res = %d: var_lli = %lld\n", res, var_lli);
+    printf("res = %d: var_lli = %lld, vad_d = %.3lf, var_e = %.2f\n", res, var_lli, var_d, var_e);
 
     return 0;
 }
