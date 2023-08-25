bc7b639ad18fdaf887b1a38c3024c33f41c2f7d8
KuzyaDomovoi <vasilyshtanko@gmail.com>
2023-08-25 16:34:12 +0300

    C

--- lesson14.c
+++ lesson14.c
@@ -2,7 +2,32 @@
 
 int main(void)
 {
+    int item;
+
+    printf("1. qwertz\n"
+           "2. qwerty\n"
+           "3. exit\n");
+
+    if(scanf("%d", &item) != 1) {
+        printf("Error input");
+        return 0;
+    }
     
+    switch(item) {
+    case 1:
+        printf("qwertz\n");
+        return 0;
+    case 2:
+        printf("qwerty\n");
+        return 0;
+    case 3:
+        printf("exit\n");
+        return 0;
+    
+    default:
+        printf("incorrect symbol");
+        return 0;
+    }
 
     return 0;
 }
