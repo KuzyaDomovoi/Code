45ebdc82576aaa03141b3e7b6b1edb853cb2f144
KuzyaDomovoi <vasilyshtanko@gmail.com>
2023-08-25 15:01:21 +0300

    C

--- .vscode/lesson11.c
+++ .vscode/lesson11.c
@@ -3,7 +3,18 @@
 
 int main(void)
 {
+    int x;
     
+    if(scanf("%d", &x) != 1) {
+        printf("Error input\n");
+        return 0;
+    }
+    
+    if(x)x = -x;
+    printf("x = %d\n", x);
+    
+    if(x >= 3 && x <= 11)
+        printf("x in [3; 11]\n");
     
     return 0;
 }
