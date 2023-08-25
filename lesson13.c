d988533c039403e7040c6808f2cc59ab38a7836e
KuzyaDomovoi <vasilyshtanko@gmail.com>
2023-08-25 15:41:59 +0300

    C

--- lesson13.c
+++ lesson13.c
@@ -1,8 +1,25 @@
 #include <stdio.h>
+#include <math.h>
 
 int main(void)
 {
+    double a = 7.5, b = -3.43;
+    double max_ab = a > b ? a : b;
+    //double max_ab = (a > b) ? a + 2 : b - 5;
+
+    /*if(a > b)
+        max_ab = a;
+    else
+        max_ab = b;
+    */
     
+    double res_abs = (a < b) ? fabs(a) : fabs(b);
+
+    int z = 8;
+    printf("z = %d is %s digit\n", z, (z % 2 == 0) ? "even" : "odd");
+    
+    printf("max_ab = %.2f\n", max_ab);
+    printf("res_abs = %.2f\n", res_abs);
         
     return 0;
 }
