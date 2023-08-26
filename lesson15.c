3a550a2ba5e5b56a9fb0397d5bab4477e65c4188
KuzyaDomovoi <vasilyshtanko@gmail.com>
2023-08-26 08:02:28 +0300

    C

--- lesson15.c
+++ lesson15.c
@@ -2,7 +2,18 @@
 
 int main(void)
 {
-    
+    unsigned char var = 153; //binary notation 10011001;
+    unsigned char not_var = ~var; //result: 01100110 (number 102);
+
+    printf("var = %d, not_var = %d\n", var, not_var);
 
     return 0;
 }
+
+//  153 + 102 = 255;
+
+//  10011001 (153)
+// +
+//  01100110 (102)
+// =
+//  11111111 (255)
