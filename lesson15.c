a5fcdebfae67cb4d0067fbd160078e1237d19e64
KuzyaDomovoi <vasilyshtanko@gmail.com>
2023-08-26 09:16:31 +0300

    C

--- lesson15.c
+++ lesson15.c
@@ -3,8 +3,8 @@
 int main(void)
 {
     unsigned char var = 153; //binary notation 10011001;
-    unsigned char not_var = ~var; //result: 01100110 (number 102);
-    //  153 + 102 = 255;
+    unsigned char not_var = ~var; // ~ (invert), result: 01100110 (number 102);
+    //  153 + 102 = 255:
     //  10011001 (153)
     // +
     //  01100110 (102)
@@ -17,7 +17,7 @@ int main(void)
     unsigned char mask = 4;      //     00000100
                                  //    = 
     unsigned res = flags & mask; //     00000100;
-    //flags & mask;
+    //flags & mask (and) (*)
     //  0 * 0 = 0;   
     //  0 * 1 = 0;
     //  1 * 0 = 0;
@@ -30,16 +30,21 @@ int main(void)
 
     flags = flags & ~mask; //     00000101 (5) flags
                            // &(*)     
-                           //     11111011 (~00000100) (4) mask
+                           //     11111011 (251) <- (~00000100) (4) mask
                            //    = 
                            //     00000001 (1) res;
     printf("flags = %d\n", flags);
 
     unsigned char flags_1 = 8; //00001000;
-    unsigned char mask_1 = 5; //00000101;
+    unsigned char mask_1 = 5;  //00000101;
 
     flags_1 = flags_1 | mask_1; // | (or) (+);
     printf("flags_1 = %d\n", flags_1);
+    //     00001000
+    // |(+)
+    //     00000101
+    //    =
+    //     00001101;
 
     return 0;
 }
