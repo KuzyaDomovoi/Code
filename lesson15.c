baefa15741dc07d2819d63dc59a368d8b1f9a8e7
KuzyaDomovoi <vasilyshtanko@gmail.com>
2023-08-26 08:51:16 +0300

    C

--- lesson15.c
+++ lesson15.c
@@ -4,29 +4,25 @@ int main(void)
 {
     unsigned char var = 153; //binary notation 10011001;
     unsigned char not_var = ~var; //result: 01100110 (number 102);
-
     //  153 + 102 = 255;
     //  10011001 (153)
     // +
     //  01100110 (102)
     // =
     //  11111111 (255);
-
     printf("var = %d, not_var = %d\n", var, not_var);
-
-    //flags & mask;
-    //  0 * 0 = 0;   
-    //  0 * 1 = 0;
-    //  1 * 0 = 0;
-    //  1 * 1 = 1;    
+       
     unsigned char flags = 5;     //     00000101
                                  // &(*)
     unsigned char mask = 4;      //     00000100
                                  //    = 
     unsigned res = flags & mask; //     00000100;
-    
-    printf("%d\n", res);
-    
+    //flags & mask;
+    //  0 * 0 = 0;   
+    //  0 * 1 = 0;
+    //  1 * 0 = 0;
+    //  1 * 1 = 1;
+    printf("res = %d\n", res);
     if((flags & mask) == mask)
         printf("bit 2 is on\n");
     else
@@ -37,7 +33,13 @@ int main(void)
                            //     11111011 (~00000100) (4) mask
                            //    = 
                            //     00000001 (1) res;
-    printf("Flags = %d\n", flags);
+    printf("flags = %d\n", flags);
+
+    unsigned char flags_1 = 8; //00001000;
+    unsigned char mask_1 = 5; //00000101;
+
+    flags_1 = flags_1 | mask_1; // | (or) (+);
+    printf("flags_1 = %d\n", flags_1);
 
     return 0;
 }
