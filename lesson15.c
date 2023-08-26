b464a00a378f674efe2bd2b1e1e3f36d1c664f9c
KuzyaDomovoi <vasilyshtanko@gmail.com>
2023-08-26 09:40:49 +0300

    C

--- lesson15.c
+++ lesson15.c
@@ -2,8 +2,10 @@
 
 int main(void)
 {
-    unsigned char var = 153; //binary notation 10011001;
-    unsigned char not_var = ~var; // ~ (invert), result: 01100110 (number 102);
+// Binary operation '~' (inverting):
+
+    unsigned char var = 153; //binary num notation is 10011001;
+    unsigned char not_var = ~var; //invert result == 01100110 (decimal num notation is 102);
     //  153 + 102 = 255:
     //  10011001 (153)
     // +
@@ -11,7 +13,9 @@ int main(void)
     // =
     //  11111111 (255);
     printf("var = %d, not_var = %d\n", var, not_var);
-       
+
+// Binary opetation '&' (and) == (*):
+
     unsigned char flags = 5;     //     00000101
                                  // &(*)
     unsigned char mask = 4;      //     00000100
@@ -35,16 +39,30 @@ int main(void)
                            //     00000001 (1) res;
     printf("flags = %d\n", flags);
 
-    unsigned char flags_1 = 8; //00001000;
+// Binary operation '|' (or) (+):
+
+    unsigned char flags_1 = 9; //00001000;
     unsigned char mask_1 = 5;  //00000101;
-
     flags_1 = flags_1 | mask_1; // | (or) (+);
-    printf("flags_1 = %d\n", flags_1);
-    //     00001000
+    //     00001001 (9)
     // |(+)
-    //     00000101
+    //     00000101 (5)
     //    =
-    //     00001101;
+    //     00001101 (13);
+    printf("flags_1 = %d\n", flags_1);
 
+
+// Binary operation '^' (exclusive 'or' - XOR):
+
+    unsigned char flags_2 = 9; //00001001;
+    unsigned char mask_2 = 1;  //00000001;
+    flags_2 = flags_2 ^ mask_2;
+    //   00001001 (9)
+    //  ^
+    //   00000001 (1)
+    //  =
+    //   00001000 (8);
+    printf("flags_2 = %d\n", flags_2);
+    
     return 0;
 }
