4fe4d5d0224fc28b4cc4bd1a2686cc03b9dd96db
KuzyaDomovoi <vasilyshtanko@gmail.com>
2023-08-26 08:09:45 +0300

    C

--- lesson15.c
+++ lesson15.c
@@ -5,15 +5,20 @@ int main(void)
     unsigned char var = 153; //binary notation 10011001;
     unsigned char not_var = ~var; //result: 01100110 (number 102);
 
+    //  153 + 102 = 255;
+    //  10011001 (153)
+    // +
+    //  01100110 (102)
+    // =
+    //  11111111 (255);
+
     printf("var = %d, not_var = %d\n", var, not_var);
+
+    unsigned char v_1 = 5;
+    unsigned char v_2 = 4;
+    
+    unsigned res = v_1 & v_2;
+    printf("%d\n", res);
 
     return 0;
 }
-
-//  153 + 102 = 255;
-
-//  10011001 (153)
-// +
-//  01100110 (102)
-// =
-//  11111111 (255)
