1cf1c1fcbc50bf1ee5a866cc93d908f2cbe248ab
KuzyaDomovoi <vasilyshtanko@gmail.com>
2023-08-28 11:33:54 +0300

    C

--- lesson18.c
+++ lesson18.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 
 #define FIVE    5
-#define TEN      2 * FIVE
+#define TEN     2 * FIVE
+#undef TEN
+#define TEN     10
 #define TEXT     "Text mesage in one line"
-#define TEXT2    "Text mesage in \
+#define TEXT2    "Text2 mesage in \
 one line"
 #define PRINT_D  printf("digit = %d\n", digit)
 #define FORMAT   "digit = %d\n"
