661273684d1f40c4415d8a01367337cfb294ccf9
KuzyaDomovoi <vasilyshtanko@gmail.com>
2023-08-28 12:38:28 +0300

    C

--- lesson20.c
+++ lesson20.c
@@ -1,8 +1,12 @@
-#include <stdio.h>
+//#include <stdio.h>
+//int printf(const char *format, ...);
+#include "tmp/printf.h" /*Warning: The names of 
+standart header files supplied with the C complier 
+should be encloused in angle brackets. The names of 
+own header files should be encloused in double quotes.*/
 
 int main(void)
 {
-    
-    
+    printf("Hello, world!\n");
     return 0;
 }
