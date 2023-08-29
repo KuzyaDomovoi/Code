4a119f9f0ab6b72b1a469c25b5e99379e18a9b19
KuzyaDomovoi <vasilyshtanko@gmail.com>
2023-08-29 10:20:41 +0300

    C

--- lesson24.c
+++ lesson24.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(void)
 {
     //5! = 1 * 2 * 3 * 4 * 5;   0! = 1; 
 
     int n = 5, p = 1;
-
     for(int i = 1; i <= n; ++i)
         p = p * i;
-
     printf("p = %d\n", p);
 
     int x, y = 1;
@@ -18,13 +17,15 @@ int main(void)
     }
     for(y; x > 0; y *= x, --x)
         { }
-    
+    printf("y = %d\n", y);
+
     /*int n = 5, i, p;
     for(i = 1, p = 1; i <= n; p = p * i, ++i)
         { }
     printf("p = %d\n", p);*/
 
-    printf("y = %d\n", y);
-
+    for(int x = rand() % 10; x != 0; x = rand() % 10)
+        printf("x = %d\n", x);
+    
     return 0;
 }
