a7c59a22a5fd5afc3235cf5fa64b0d1f4832c1bc
KuzyaDomovoi <vasilyshtanko@gmail.com>
2023-08-29 13:45:33 +0300

    C

---
+++ practice25.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int I, J, K, S;
+    
+    for(int i = -3; i <= 7; I += i, ++i)
+        { }
+        printf("I = %d\n", I);
+        for(int j = 1; j <= 5; J += j, ++j)
+            { }
+            printf("J = %d\n", J);
+            for(int k = 2; k <= 4; K += k, ++k)
+                { }
+                printf("K = %d\n", K);
+
+    S = (I + J - K) * (I + J - K);
+    printf("S = %d\n", S);
+
+    return 0;
+}
