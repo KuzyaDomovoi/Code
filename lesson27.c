456416315212cce453a707df9e8cd310bbf16188
KuzyaDomovoi <vasilyshtanko@gmail.com>
2023-08-30 19:28:50 +0300

    C

--- lesson27.c
+++ lesson27.c
@@ -3,8 +3,8 @@
 int main(void)
 {
 /* Pointers are designed to write and read data from any
-mamory cells and store the addresses of cells with data.
-They are denotet witn the sign '*', for example, as 
+memory cells and store the addresses of cells with data.
+They are denoted witn the sign '*', for example, as 
 'char *gpt' and divided into the following variable types: 
     char *gpt;   (memory area is 1 byte),
     int *gpt;    (memory area is 4 bytes),
@@ -25,14 +25,14 @@ Pointer notation construction:
     printf("gpt = %p, *gpt = %d, d = %d\n", gpt, *gpt, d);
 
     
-    int c = 10; // for data type 'int': 10 = 10 + 256^1 * 0 + 256^2 * 0 + 256^3 * 0 (is written sequentially to four memory cell (4 bytes));
-    int *gpt = &c;
+    int f = 10; // for data type 'int': 10 = 10 + 256^1 * 0 + 256^2 * 0 + 256^3 * 0 (is written sequentially to four memory cell (4 bytes));
+    int *gpd = &f;
 
-    printf("gpt = %p, *gpt = %d, c = %d\n", gpt, *gpt, c);
+    printf("gpt = %p, *gpt = %d, f = %d\n", gpd, *gpd, f);
 
-    *gpt = 75432; // int: 75432 = 168 + 256^1 * 38 + 256^2 * 1 + 256^3 * 0;
+    *gpd = 75432; // int: 75432 = 168 + 256^1 * 38 + 256^2 * 1 + 256^3 * 0;
     //*gpt = 1; // int: 1 = 1 + 256^1 * 0 + 256^2 * 0 + 256^3 * 0;
-    printf("gpt = %p, *gpt = %d, c = %d\n", gpt, *gpt, c);
+    printf("gpt = %p, *gpd = %d, f = %d\n", gpd, *gpd, f);
 
     return 0;
 }
