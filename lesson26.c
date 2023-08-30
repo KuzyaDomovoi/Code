9c3f14dc1ad55c23d68ddf6c34057e7fa7dd8a8e
KuzyaDomovoi <vasilyshtanko@gmail.com>
2023-08-30 10:29:31 +0300

    C

--- lesson26.c
+++ lesson26.c
@@ -3,9 +3,14 @@
 int main(void)
 {
     double x, s = 0.0;
-    while(scanf("%lf", &x) == 1) {
-        if(x < 0)
-            break;
+    while(scanf("%lf", &x) == 1 && x > 0) {
+    //    if(x < 0)
+    //        break; 
+    /*The statment 'break' terminates only the current 
+    loop 'while' (in the current function 'main') and 
+    transfers to the next statment.
+    But if the statement 'return' is used, the current 
+    funktion 'main' is terminated.*/
         s += x;
     }
     printf("s = %.2f\n", s);
