e1178f0ce9a4377815d386f2156c5888dda3d560
KuzyaDomovoi <vasilyshtanko@gmail.com>
2023-08-30 13:56:07 +0300

    C

--- lesson26.c
+++ lesson26.c
@@ -47,13 +47,21 @@ int main(void)
     for(int i = 1; i <= 10; ++i)
         for(int j = 7; j >= 5; --j) {
             if( i - j > 0)
-                goto exit_sum; //break;
+                goto exit_sum; //break, return;
     /*The 'goto' statement is used to break all nested
     loop statements ('for') and transfer to the specified
-    lable.
+    lable ('exit_sum').
     Using the 'break' statement hier allows to break only 
     internal loop statement and all external loop 
-    tatements remain in operation.*/
+    tatements remain in operation.
+    Using the 'return' statement hier allows to break all 
+    nested loop statement, but the current function ('main')
+    is also breaked.
+    ! The frequent use of the 'goto' statement makes the
+    progtamm more difficult to read und edit. And at 
+    the compile time the executable code will not be 
+    optimazed for fast execution on the current computer
+    architecture.*/
             s += i - j;
         }
     exit_sum: printf("s = %d\n", s);
