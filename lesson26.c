3968636cb1a36815267b03dbe61b721f3ae59d30
KuzyaDomovoi <vasilyshtanko@gmail.com>
2023-08-30 11:19:36 +0300

    C

--- lesson26.c
+++ lesson26.c
@@ -2,7 +2,7 @@
 
 int main(void)
 {
-    double x, s = 0.0;
+    double x, sum_1 = 0.0;
     while(scanf("%lf", &x) == 1 && x > 0) {
     //    if(x < 0)
     //        break; 
@@ -12,11 +12,22 @@ int main(void)
     function 'main'.
     But the statement 'return' is used to terminate 
     the call to the current funktion 'main'. That is 
-    it can interrupt any following statment within the 
+    it can interrupt any subsequent statment within the 
     current function.*/
-        s += x;
+        sum_1 += x;
     }
-    printf("s = %.2f\n", s);
+    printf("sum_1 = %.2f\n", sum_1);
+
+    int y, sum_2 = 0;
+    while(scanf("%d", &y) == 1 && y != 0) {
+        if(y % 2 == 0)
+            continue;
+    /*The statement 'continue' is used to skip the all
+    subsequent oparations within the current iteration and 
+    transfer to top of the current loop.*/
+        sum_2 += y;
+    }
+    printf("sum_2 = %d\n", sum_2);
 
     return 0;
 }
