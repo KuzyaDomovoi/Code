c039538e4e2f84dd981386e2eff010489004f3b1
KuzyaDomovoi <vasilyshtanko@gmail.com>
2023-08-30 10:54:37 +0300

    C

--- lesson26.c
+++ lesson26.c
@@ -8,11 +8,12 @@ int main(void)
     //        break; 
     /*The statment 'break' is used to prematurely 
     terminatation only the current loop 'while' and
-    transfers to the next statment.
-    But if the statement 'return' is used, the call to the 
-    current funktion 'main' is terminated. That is it can 
-    interrupt any following statment within the current 
-    function.*/  
+    transfers to the next statment within the current 
+    function 'main'.
+    But the statement 'return' is used to terminate 
+    the call to the current funktion 'main'. That is 
+    it can interrupt any following statment within the 
+    current function.*/
         s += x;
     }
     printf("s = %.2f\n", s);
