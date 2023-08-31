024b363938bd8598493da86ec08fd89c5be0f329
KuzyaDomovoi <vasilyshtanko@gmail.com>
2023-08-31 13:34:40 +0300

    C

--- lesson32.c
+++ lesson32.c
@@ -12,7 +12,26 @@ int main(void)
         //ptr++; 
         /*If the prefix form '*++ptr', than increment 
         operation is perfomed first and than the 
-        declaration of pointer.*/
+        deference operation.*/
 
-   return 0;
+    int f = 476789;
+    int *p = &f;
+    *p += 1;
+    printf("\nf = %d\n", f); // f = 47690;
+
+    int h = 476789;         // 117 + 256^1 * 70 + 256^2 * 7;
+    char *d = (char *)&h;   // 117, 70, 7, 0;
+    int x = *d + 1;         // 117 + 1;
+    printf("\nh = %d, x = %d\n", h, x);     // h = 476789, x = 118;
+
+    int j = 476789;         // 117 + 256^1 * 70 + 256^2 * 7;
+    char *k = (char *)&j;   // 117, 70, 7, 0;
+    int y = *(k + 1);       // 70;
+    printf("\nj = %d, y = %d\n", j, y);     // j = 476789, y = 70;
+
+    int l = 476789;         // 117 + 256^1 * 70 + 256^2 * 7;
+    char *b = (char *)&l;   // 117, 70, 7, 0;
+    int z = (*b)++;         // 117;
+    printf("\nl = %d, z = %d\n", l, z);     // l = 476790, z = 117
+    return 0;
 }
