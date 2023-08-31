faabe122ddbbdb6131fcf317d3ac38f2798f6ca5
KuzyaDomovoi <vasilyshtanko@gmail.com>
2023-08-31 09:48:52 +0300

    C

--- lesson29.c
+++ lesson29.c
@@ -5,9 +5,12 @@ int main(void)
     int arg = 777; // 777 = 9 + 256^1 * 3;
     int *ptr_arg;
     char *ptr; // ! warning: assignment to 'char *' from incompatible type 'int *';
+    void *p; // general type 'void *', but it is not to use with memory cells (*p = 10; : error: invalid use of void expression);
 
     ptr_arg = &arg;
     ptr = (char *)ptr_arg; // assigning the variable 'ptr_arg' to compatible type 'char *'; 
+    p = ptr;
+    ptr_arg = p;
 
     *ptr = 1; // 1 + 256^1 * 3 = 769; the value '1' is writted to the first of four memory cells only and and changes the 'arg' value 777 to 769;
     
