2d4038cdc10e565985404a9ac98d9c213e3cea1c
KuzyaDomovoi <vasilyshtanko@gmail.com>
2023-09-01 11:41:06 +0300

    c

--- lesson36.c
+++ lesson36.c
@@ -1,19 +1,39 @@
 #include <stdio.h>
-#include <stdlib.h>
-
-#define TOTAL_MARKS 13
-
-int month[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
 int main(void)
 {
-    int val = -5;    
-    int marks[TOTAL_MARKS] = {1, 2, 3, 4, val, abs(val), 0}; //array initialization, not declaration;
-    marks[10] = 10;
-    for(int i = 0; i < sizeof(marks) / sizeof(marks[0]); ++i)
-        printf("%d ", marks[i]); // 1 2 3 4 -5 5 0 0 0 0 10 0 0;
-    
+    short ar[] = {4, 3, 2, 1, 5, 6, 7};
 
+    short a_1 = *ar; // 4
+    short a_4 = *(ar + 3); // 1
+    printf("a_1 = %d, a_4 = %d\n", a_1, a_4);
+
+    for(int i = 0; i < sizeof(ar) / sizeof(*ar); ++i)
+        printf("%d ", *(ar + i)); // 4 3 2 1 5 6 7;
+    
+    printf("\n");
+
+/*- the sizeof operation  returns a numbers of bytes
+occupied by the array in the device's memory to array 
+pointer.
+- the array pointer address is formed at the moment of 
+its declaration and remains constant while program is 
+running.*/
+
+    short *ptr_ar;
+    ptr_ar = ar; // ptr _ar = &ar[0];
+    short x = *ptr_ar;
+    *(ptr_ar + 1) = -3;
+    printf("x1 = %d , x2 = %d\n", x, *(ptr_ar +1));
+
+    size_t len_1 = sizeof(ar); // variable len_ 1 stores the size of all array elements;
+    size_t len_2 = sizeof(ptr_ar); // variable len_ 3 stores the size of element 'ptr_ar' in array only;
+
+    // ar++; // the address of pointer 'ar' is not changeable;
+    ptr_ar++; // but the address of pointer 'ptr_ar' only is changeable;
+
+    
 
     return 0;
 }
+
