b9bf771f1479dab88e4fd570c81ebdc08625135d
KuzyaDomovoi <vasilyshtanko@gmail.com>
2023-09-01 11:53:47 +0300

    C

--- lesson37.c
+++ lesson37.c
@@ -3,21 +3,11 @@
 int main(void)
 {
     short ar[] = {4, 3, 2, 1, 5, 6, 7};
-
-    
-    for(int i = 0; i < sizeof(ar) / sizeof(*ar); ++i)
-        printf("%d ", *(ar + i)); // 4 3 2 1 5 6 7;
-    
-    printf("\n");
-
     short *ptr_ar;
-    ptr_ar = ar;
-    short x = *ptr_ar;
-    *(ptr_ar + 1) = -3;
-     
-    short z = ptr_ar[2];
 
-    printf("z = %d\n", z);
+    ptr_ar = ar;
+    
+    short z = ptr_ar[2];
 
     return 0;
 }
