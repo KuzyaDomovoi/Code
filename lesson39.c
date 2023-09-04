b1e5e9587b1f1d8b2ea72a1be7e42be7770f7714
KuzyaDomovoi <vasilyshtanko@gmail.com>
2023-09-04 11:51:25 +0300

    C

--- lesson39.c
+++ lesson39.c
@@ -1,19 +1,21 @@
 #include <stdio.h>
 
+#define TOTAL_MARKS 10
+
 int main(void)
 {
-    float func_1[100] = {2.4, -3.8, 0, 10.2, 11.78, -5.43};
-    int func_2[50]; // float; double;
+    int marks[TOTAL_MARKS] = {3, 2, 5};
+    int insert_index = 2;
+    
+    for(int i = TOTAL_MARKS - 1; i > insert_index; --i) {
+        marks[i] = marks[i - 1];
+//      printf("marks[%d] = marks[%d]\n", i, i - 1);
+    }
 
-    int size_1 = sizeof(func_1) / sizeof(func_1[0]);
-    int size_2 = sizeof(func_2) / sizeof(func_2[0]);
-    int size = (size_1 < size_2) ? size_1 : size_2;
+    marks[insert_index] = 4;
 
-    for(int i = 0; i < size; ++i)
-        func_2[i] = (int)func_1[i];
-
-    for(int i = 0; i < size; ++i)
-        printf("%d ", func_2[i]); // "%.2f ";
+    for(int i = 0; i < TOTAL_MARKS; ++i)
+        printf("%d ", marks[i]);
 
     return 0;
 }
