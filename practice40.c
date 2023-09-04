55d798c5f91668ed4f9e4c509da0bea33106daaf
KuzyaDomovoi <vasilyshtanko@gmail.com>
2023-09-04 20:23:53 +0300

    C

--- practice40.c
+++ practice40.c
@@ -4,23 +4,28 @@ int main(void)
 {
     char ar[] = {8, 4, 1, 3, 6, 7, 9, 0, 2, 6};
     int size = sizeof(ar) / sizeof(ar[0]);
-    printf("size = %d\n", size);
     int repit;
     int pos;
     
     for(int i = 0; i < size; ++i) {
         repit = i;
         for(int j = i + 1; j < size; ++j) {
-            printf("i = %d , j = %d , repit = %d\n", i, j, repit);
             if(ar[repit] != ar[j])
-                repit = i;
-            else 
+                repit = i; 
+            else
                 for(int k = repit; k < size; ++k) {
                     ar[k] = ar[k + 1];
-                    printf("ar[%d] = ar[%d]\n", k, k + 1);
                 }
         }
     }
+
+    for(int i = 0; i < size; ++i)
+        printf("%d ", ar[i]);
+        printf("\n");
+
+    int ins = 9;
+    ar[ins] = 5;
+          
     for(int i = 0; i < size; ++i)
         printf("%d ", ar[i]);
         printf("\n");
@@ -30,7 +35,6 @@ int main(void)
         for(int j = i + 1; j < size; ++j) {
             if(ar[pos] > ar[j])
                 pos = j;
-                printf("i %d, j %d, pos %d\n", i, j, pos);
         }       
             if(pos != i) {
                 int t = ar[i];
