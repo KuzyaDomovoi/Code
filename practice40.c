b572928cea868d8dd54b015c8b616d5999337996
KuzyaDomovoi <vasilyshtanko@gmail.com>
2023-09-04 19:23:45 +0300

    C

--- practice40.c
+++ practice40.c
@@ -4,29 +4,33 @@ int main(void)
 {
     char ar[] = {8, 4, 1, 3, 6, 7, 9, 0, 2, 6};
     int size = sizeof(ar) / sizeof(ar[0]);
+    printf("size = %d\n", size);
     int repit;
     int pos;
     
-    for(int i = 0; i < size -1; ++i) {
+    for(int i = 0; i < size; ++i) {
         repit = i;
         for(int j = i + 1; j < size; ++j) {
-            if(ar[repit] = ar[j])
-                repit = j;
-        }    
-        for(int k = repit; k < size - 1; ++k) {
-            ar[k] = ar[k + 1];
-            printf("ar[%d] = ar[%d]\n", i, i + 1);
+            printf("i = %d , j = %d , repit = %d\n", i, j, repit);
+            if(ar[repit] != ar[j])
+                repit = i;
+            else 
+                for(int k = repit; k < size; ++k) {
+                    ar[k] = ar[k + 1];
+                    printf("ar[%d] = ar[%d]\n", k, k + 1);
+                }
         }
     }
     for(int i = 0; i < size; ++i)
         printf("%d ", ar[i]);
         printf("\n");
 
-    for(int i = 0; i < size - 1; ++i) {
+    for(int i = 0; i < size; ++i) {
         pos = i;
         for(int j = i + 1; j < size; ++j) {
             if(ar[pos] > ar[j])
                 pos = j;
+                printf("i %d, j %d, pos %d\n", i, j, pos);
         }       
             if(pos != i) {
                 int t = ar[i];
