5ff1485c52c29fdad07550ce00ee857f4077fcde
KuzyaDomovoi <vasilyshtanko@gmail.com>
2023-09-05 15:18:07 +0300

    C

--- lesson45.c
+++ lesson45.c
@@ -2,7 +2,24 @@
 
 int main(void)
 {
-    
+// Strings:
+
+/* in C language is no data of type 'string' therefor
+an array of type 'char' is used here to store a sequence
+of characters.*/
+
+    char str[] = {'B', 'a', 'l', 'a', 'k', 'i', 'r', 'e', 'v', '\0'};
+    char s[] = "Sergey Balakirev";
+    char buffer[512] = "Hello, World";
+
+    char b[] = "Hel" "lo"
+                ", World";
+
+    char sp[] = "Hello, \
+World";
+
+    char string[] = "a"; // is a string, no a symbol, becouse is used here double quotes;
+    char sym = 'a'; // is a symbol, because - single quotes;
 
     return 0;
 }
