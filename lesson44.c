301960fd018b1bf8105825a89e9796cb4a063b69
KuzyaDomovoi <vasilyshtanko@gmail.com>
2023-09-05 14:44:57 +0300

    C

---
+++ lesson44.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+{
+// Pointers to 2d array:
+
+    char game_pole[3][3] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    char *p_row = game_pole[1]; // index [1] is the second string of 2d array: 4, 5, 6; 
+    printf("%d\n", *p_row); // 4 is an address (the first element of string) of the second 1d array string;
+    printf("%d\n", *(p_row - 2)); // 2 ('-' is a shift by two elements to the left);
+
+    char (*p_ar)[3] = game_pole; // parentheses are required here, becouse it is the pointer to an element with index 3 of 2d array (game_pole);
+    //char *ptr[3]; // is not 2d array pointer! If no parentheses hier it is the array of three ([3]) pointers, becouse priority of index '[3]' is higher than priority of '*'; 
+
+    char x = p_ar[0][1]; // the variable x saves a value '2' (the second element of first array row '1, 2, 3' (1d array));
+
+    return 0;
+}
