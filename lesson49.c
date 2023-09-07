4a8d2b72dc156575b2ae43237a03577ff9eddf93
KuzyaDomovoi <vasilyshtanko@gmail.com>
2023-09-07 16:13:32 +0300

    C

--- lesson49.c
+++ lesson49.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <string.h>
+#include <string.h> // to use the functions: strcmp() and strncmp();
 
 int main(void)
 {
@@ -14,13 +14,27 @@ int main(void)
 
     int i = 0; // auxiliary loop's counter;
     for(; str1[i] != '\0' && str2[i] != '\0'; ++i) // iterate the characters of strings (str1 and str2) until one of them will be != '\0';
-        if(str1[i] != str2[i]) { // checking if a current char of str1 is not equal a current char of str2 (so will be checked is lengths of the two strings not equal);
+        if(str1[i] != str2[i]) { // checking if a current char of str1 is not equal a current char of str2;
             puts("Strings are not equal!");
+            return 0;
         }
-    if(str1[i] != str2[i]) { // checking if characters of current strings are not equal;
+    if(str1[i] != str2[i]) { // checking current chars of two strings and they lengths are not equal;
         puts("Srting length are not equal!");
+        return 0;
     }
     puts("Strings are equal!");
 
-    return 0;
+    if(strcmp(s1, s2) == 0) // if == 0 than strs are equal, if < 0 than s1 < s2 and if > 0 than s1 > s2;
+        puts("Strings are equal!");
+    else
+        puts("Strings are not equal!");
+
+    const char *strings[] = { // constable pointer's array;
+        "Ship", "Shopping", "Shematic", "Super", "Car", "Sherif"
+    };
+    for(int i = 0; i < sizeof(strings) / sizeof(*strings); ++i)
+        if(strncmp(strings[i], "Sh", 2) == 0)
+            puts(strings[i]);
+
+        return 0;
 }
