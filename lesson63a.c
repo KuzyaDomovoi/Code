6ff5700a18765a86ba65d161d77da71ead7552a4
KuzyaDomovoi <vasilyshtanko@gmail.com>
2023-09-15 10:59:52 +0300

    C

---
+++ lesson63a.c
@@ -0,0 +1,31 @@
+#include <iostream> // in C++;
+
+int modul(int x)
+{
+    return (x < 0) ? -x : x;
+}
+
+double modul(double x) // the func names can be the same but the datatypes are different;
+{
+    return (x < 0) ? -x : x;
+}
+
+void show_args(short a = 10, double b = 20.0, int c = 30) // here can be passed defoult values in func; 
+{
+    printf( "s = %d, b = %.2f, c = %d\n", a, b, c);
+}
+
+int main(void)
+{
+    int a1 = modul(-3);
+    int a2 = modul(-3.5);
+    double a3 = modul(-3);
+    double a4 = modul(-3.5);
+
+    show_ args();
+    show_ args(1);
+    show_ args(1, 2);
+    show_ args(1, 2, 3);
+
+    return 0;
+}
