e973f5ac3b55766ca0eddd9e05c752575a3ec68a
KuzyaDomovoi <vasilyshtanko@gmail.com>
2023-09-16 13:31:32 +0300

    C

--- lesson67.c
+++ lesson67.c
@@ -6,7 +6,7 @@ extern int global_var; // the var is declared in the file 'modul.c'
 extern char global_str[]; // only description of global var;
 extern int global_seed_randint;
 
-int randint(int, int); // onle description of prototype func and is an equivalent of keyword 'extern' for global vars;
+int randint(int, int); // only description of prototype func and is an equivalent of keyword 'extern' for global vars;
 //extern int randint(int, int); is equal int randint(int, int); 
 
 // Keyword static:
