9620fb27585541b22abf73348e1f5d82a1375753
KuzyaDomovoi <vasilyshtanko@gmail.com>
2023-09-16 13:29:39 +0300

    C

--- lesson67.c
+++ lesson67.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 
+extern int global_var; // the var is declared in the file 'modul.c'
+/*'extern' is not a declaration but a description of var, which later will be declared both in the current file or in an another file (modul.c);*/
+
+extern char global_str[]; // only description of global var;
+extern int global_seed_randint;
+
+int randint(int, int); // onle description of prototype func and is an equivalent of keyword 'extern' for global vars;
+//extern int randint(int, int); is equal int randint(int, int); 
+
 // Keyword static:
 /*it locates addresses of vars in RAM .data or .bss section
 and allows the vars to take up large volumes of memory;*/
@@ -27,7 +36,18 @@ int main(void)
     printf("times = %u\n", times); // 11;
     printf("counter(): %u\n", counter()); // 12;
     printf("counter(): %u\n", counter()); // 13;
-    printf("counter(): %u\n", counter()); // 14; 
+    printf("counter(): %u\n", counter()); // 14;
+
+    printf("global_var = %d\n", global_var); // 5;
+    printf("global_str = %s\n", global_str); // Hello;
     
+    int a = 1, b = 10; // definition of range(a, b) for prototype func 'int randint';
+    printf("%d\n", randint(a, b)); // 4;
+    printf("%d\n", randint(a, b)); // 7;
+    printf("%d\n", randint(a, b)); // 8;
+
+    printf("%d\n", global_seed_randint); // 0;
+
     return 0;
 }
+char global_str[100] = "Hello"; // definition the global var; 
