26f1d65d5b191cde316f4e07bb76fde88b4f151d
KuzyaDomovoi <vasilyshtanko@gmail.com>
2023-09-16 13:34:12 +0300

    C

--- lesson67.c
+++ lesson67.c
@@ -30,7 +30,10 @@ int main(void)
     int var_main; // automatic local var;
     static int var_st;
 
-    static double big_arg[10000000]; // exit with code = 0 becouse before the local var's 'big-ar' datatype is assigned the keyword 'static' and therefore it been located in RAM .bss section;
+    static double big_arg[10000000]; /* exit with code = 0
+    becouse before the local var's 'big-ar' datatype is
+    passed the keyword 'static' and therefore it been
+    located in RAM .bss section;*/
 
     printf("var_main = %d, var_st = %d\n", var_main, var_st);
     
