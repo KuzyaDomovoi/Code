93fee8c84a17a6f343c2aae71de437951fa49cbe
KuzyaDomovoi <vasilyshtanko@gmail.com>
2023-09-16 13:30:37 +0300

    C

--- modul.c
+++ modul.c
@@ -3,7 +3,7 @@
 int global_var = 5; // definition the global var;
 
 int global_seed_randint = 0; 
-// static int global_seed_randint = 0; // the keyword 'static' restricts here the use of the global var, which is already static, in another moduls or files;
+// static int global_seed_randint = 0; // the keyword 'static' restricts here the use of the global var (which is already static) in another moduls or files;
 
 //static int randint(int a, int b) // the keyword 'static' restricts here the use of func in another moduls or files;
 int randint(int a, int b) // returns integer values in range(a, b);
