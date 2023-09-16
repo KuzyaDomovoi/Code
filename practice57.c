a6a40a8f4f75b154ef1ac51b6d29d4384bf35cef
KuzyaDomovoi <vasilyshtanko@gmail.com>
2023-09-16 18:59:07 +0300

    C

--- practice57.c
+++ practice57.c
@@ -4,15 +4,14 @@
 void show_ar1Dto2D(const short (*ar), int length)
 {
     int res_ij;
-    int rows = (length + (COLS - 1)) / COLS
-    for(int i = 0; i < length / COLS; ++i) { // (i <= length / COLS);
+    int rows = (length + (COLS - 1)) / COLS;
+    for(int i = 0; i < rows; ++i) { // (i <= length / COLS);
         for(int j = 0; j < COLS && i * COLS + j < length; ++j) {
             res_ij = ar[i * COLS + j];
             printf("%d ", res_ij);
         }
         putchar('\n');
     }   
-
 }
 
 int main(void)
