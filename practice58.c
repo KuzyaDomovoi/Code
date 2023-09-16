f26058c2a9ea70b48fa7d88ea43c13914cfbc23d
KuzyaDomovoi <vasilyshtanko@gmail.com>
2023-09-16 19:14:16 +0300

    C

--- practice58.c
+++ practice58.c
@@ -5,7 +5,7 @@
 void array_to_matrix(int *matrix, int *arr, int rows, int cols) {
     for (int i = 0; i < rows; ++i) {
         for (int j = 0; j < cols; ++j) {
-            arr[i * cols + j] = *(matrix + i * cols + j);
+            arr[i * cols + j] = (*matrix + i * cols + j);
         }
     }
 }
@@ -20,7 +20,7 @@ int main(void)
 {
     int matrix[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
     int ar[COLS * ROWS] = {0};
-    array_to_matrix((int *)matrix, ar, ROWS, COLS);
+    array_to_matrix(matrix, ar, ROWS, COLS);
     print_array(ar, COLS * ROWS);
     return 0;
 }
