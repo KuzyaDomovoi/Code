e3eef87fe575fce8a8ff76c7220099f5e5082975
KuzyaDomovoi <vasilyshtanko@gmail.com>
2023-09-18 15:29:26 +0300

    C

--- lesson69_3typedef.c
+++ lesson69_3typedef.c
@@ -3,11 +3,11 @@
 // Typedef:
 
 //#define BYTE unsigned char // is processed by preprocessor;
-//typedef unsigned char BYTE; // unlike the '#define' the 'typedef' statement is processed by the complier itself;
+//typedef unsigned char BYTE; // unlike the '#define' directive the 'typedef' statement is processed by the complier itself;
 
-#define PTR_INT int*
+#define PTR_INT int* // directive define;
 
-typedef int* PTR;
+typedef int* PTR; // typedef statement;
 
 typedef enum {
     buffer_size = 2048,
@@ -20,7 +20,7 @@ int main(void)
     //BYTE byte;
     //BYTE ch, var_ch = '\0';
     
-    PTR_INT a, b; // with using the '#define' statement are here pointer 'int* a' and var 'b' only (not 'int* b');
+    PTR_INT a, b; // with using the '#define' sdirective are here pointer 'int* a' and var 'b' only (not 'int* b');
     PTR ptr_a, ptr_b; // with using the 'typedef' statement both vars are pointers: int *ptr_a and int *ptr_b;
     
     SIZE_CONSTS sizes = window_size; // var 'sizes' has here the datatype enum;
