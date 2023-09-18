5fa49c9b8c5b3d45a4680c6a6232d4f70a78ddf4
KuzyaDomovoi <vasilyshtanko@gmail.com>
2023-09-18 15:44:55 +0300

    C

--- lesson69_1enum.c
+++ lesson69_1enum.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
-// Enumeration: 'enum'; The keyword enum is using to enumeration vars which are integer const data type whlie the compilation is started;
+// Enumeration: 'enum'; The keyword enum is using to enumeration vars which are integer constants whlie the compilation is started;
 
-enum colors {red, green, blue};
+enum colors {red, green, blue}; // enum is a keyword; colors is a name of enum; red ,green and blue are integer constants of enum;
 enum keys {vk_enter = 27, vk_space, vk_del = 30};
 enum {
     go = 0x1f00,
@@ -16,7 +16,7 @@ int main(void)
 {
     printf("red = %d, green = %d, blue = %d\n", red, green, blue);
     
-    enum keys k_var = vk_enter + 2 - vk_space * 100; // the var 'k_var' is an int datatype while the compilation and can take any value;
+    enum keys k_var = vk_enter + 2 - vk_space * 100; // the var 'k_var' is an int datatype var while the compilation and can take any value;
 
     switch (k_var) {
     case vk_enter:
