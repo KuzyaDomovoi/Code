778c2a5b122f1352a2215af62a6d5bb9e4236298
KuzyaDomovoi <vasilyshtanko@gmail.com>
2023-09-18 15:06:59 +0300

    C

--- lesson69_4.c
+++ lesson69_4.c
@@ -12,17 +12,16 @@ typedef char (*PTR_AR_2D)[4];
 
 int main(void)
 {
-   char ar_2d[5][4] = {0};
-   PTR_AR_2D ptr_2d = ar_2d;
+    PTR_EVEN func_even = is_even;
 
-   PTR_EVEN func_even = is_even;
+    printf("%d\n", func_even(2));
+    printf("%d\n", func_even(3));   
+
+    char ar_2d[5][4] = {0};
+    PTR_AR_2D ptr_2d = ar_2d;
+    ptr_2d[1][2] = 5;
    
-   ptr_2d[1][2] = 5;
-
-   printf("%d\n", func_even(2));
-   printf("%d\n", func_even(3));
-
-   printf("%d\n", ptr_2d[1][2]);
+    printf("%d\n", ptr_2d[1][2]);
 
     return 0;
 }
