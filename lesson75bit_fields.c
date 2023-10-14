0c3b33bae54359c9cd784913a1225da467eaec32
KuzyaDomovoi <vasilyshtanko@gmail.com>
2023-10-14 14:32:26 +0300

    C

--- lesson75bit_fields.c
+++ lesson75bit_fields.c
@@ -11,16 +11,17 @@ struct date_time {
     unsigned hour : 5;
 };
 
-// 5 + 4 + 12 + 6 + 6 + 5 = 32 + 6 (bit) => 4 + 4 = 8 (byte);
+// 8 + 5 + 4 + 12 + 6 + 6 + 5 = 32 + 14 (bit) => 4 + 4 = 8 (byte);
 
 int main(void)
 {
     struct date_time dt;
     struct date_time dtime = {3, 5, 2023, 11, 7, 10};
-
-    printf("%ld\n", sizeof(dt));
-    printf("%02d/%02d/%d %02d:%02d:%02d\n", 
-        dtime.day, dtime.mounth, dtime.year, dtime.hour, dtime.min, dtime.sec);
+    char week[] = "sat";
+    
+    printf("%ld\n", sizeof(dtime));
+    printf("%s %02d/%02d/%d %02d:%02d:%02d\n", 
+        week, dtime.day, dtime.mounth, dtime.year, dtime.hour, dtime.min, dtime.sec);
 
     return 0;
 }
