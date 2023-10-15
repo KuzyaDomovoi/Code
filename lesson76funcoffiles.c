dda0c4d6a8b2f5bc6792d5dbb5f806799a3f54fe
KuzyaDomovoi <vasilyshtanko@gmail.com>
2023-10-15 15:22:00 +0300

    C

--- lesson76funcoffiles.c
+++ lesson76funcoffiles.c
@@ -16,9 +16,11 @@ a+ 	Append and read (append or create). End of the file;
 
 int main(void)
 {
-    char str[] = "Function fputc() in action.";
-    char str_2[] = "Function fputc() in action with a-acces.";
-    char buff[100]; // decl var buff for 100 chars;
+    char str[] = "Function fputc() in action with w-acces.";
+    char str_2[] = "Function fputc() in action with w+-acces.";
+    char str_3[] = "Function fputc() in action with a-acces.";
+    char str_4[] = "Function fputc() in action with a+-acces.";
+    char buff[200]; // decl var buff for 100 chars;
     
     FILE* fp = fopen("my_file.txt", "w"); // create a FILE my_file.txt with w-acces;
     if(fp == NULL) // checking fileopening for errors; 
@@ -26,17 +28,31 @@ int main(void)
     for(int i = 0; i < str[i]; ++i) // iterate i-data(chars) to reading the var str;
         fputc(str[i], fp); // put the chars of the var str in the FILE 'fp';
     fclose(fp); // clouse the FILE 'fp' and store its data in file stream buffer;
+
+     FILE* fpp = fopen("my_file.txt", "w"); 
+    if(fpp == NULL)
+        return 2; 
+    for(int i = 0; i < str_2[i]; ++i) 
+        fputc(str_2[i], fpp);
+    fclose(fpp);
     
     FILE* fa = fopen("my_file.txt", "a");
     if(fa == NULL)
-        return 2;
-    for(int i = 0; i < str_2[i]; ++i)
-        fputc(str_2[i], fa);
+        return 3;
+    for(int i = 0; i < str_3[i]; ++i)
+        fputc(str_3[i], fa);
     fclose(fa);
+
+    FILE* faa = fopen("my_file.txt", "a+");
+    if(faa == NULL)
+        return 4;
+    for(int i = 0; i < str_4[i]; ++i)
+        fputc(str_4[i], faa);
+    fclose(faa);
 
     FILE* in = fopen("my_file.txt", "r"); // read the FILE 'my_file.txt' with r-acces;
     if(in == NULL) // checking fileopening for errors;
-        return 3;
+        return 4;
 
     char ch; // declaration a new tenmorary variable ch;
     int i = 0; // declaration a new temporary variable i = 0 (counter);
