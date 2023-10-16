20ef681c5fdeecb585abe8ae19d0c7c6745c9b6b
KuzyaDomovoi <vasilyshtanko@gmail.com>
2023-10-16 10:42:06 +0300

    C

--- lesson61rcsfuncs.c
+++ lesson61rcsfuncs.c
@@ -9,8 +9,8 @@ and is used to iterate some hierarhical binary data or
 directories and files which have complex structure or when
 the use of loops is very difficult;
 Recurcion is slower then loops!
-Recutsion depth is not endless and limited by steak
-frame memory section. Therefore, if the max rcs depth is
+Recursion depth is not endless and limited by steak
+frame memory section. Therefore when the max rcs depth is
 difficult to estimate it will be better to refuse it!*/
 
     printf("Down: x = %d\n", x);
