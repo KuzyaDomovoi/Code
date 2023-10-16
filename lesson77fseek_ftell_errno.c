26a31a9835a5af797581faaff2bd91bdc3ba35fa
KuzyaDomovoi <vasilyshtanko@gmail.com>
2023-10-16 11:33:18 +0300

    C

---
+++ lesson77fseek_ftell_errno.c
@@ -0,0 +1,27 @@
+#include <stdio.h> // funcs: stderr, fseek, ftell;
+#include <errno.h> // to use func errno;
+
+int main(void)
+{
+    char buff[100]; 
+
+    FILE* in = fopen("my_file.txt", "r"); 
+    if(in == NULL) { 
+        printf("errno: %d\n", errno);
+        perror("my_file2.txt");
+        return 1;
+    }
+    
+    fseek(in, 0, SEEK_END); // seeks to a certain position on stream;
+    int length = ftell(in); // returns current file-position of stream from whence(SEEK_END);
+
+    for(int i = 1; i <= length; ++i) { // read all bytes from stream(file 'in');
+        fseek(in, -i, SEEK_END); // sets the file position in stream from end to beginning of file 'in';
+        putchar(fgetc(in)); // reads and outputs on dysplay the current char in file 'in';
+    }
+    
+    putchar('\n');
+    fclose(in); 
+    
+    return 0;
+}
