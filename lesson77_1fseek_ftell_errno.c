ad441268bc890f3a2162d4af143b5fec6c9eab57
KuzyaDomovoi <vasilyshtanko@gmail.com>
2023-10-16 11:44:29 +0300

    C

--- lesson77_1fseek_ftell_errno.c
+++ lesson77_1fseek_ftell_errno.c
@@ -16,7 +16,7 @@ int main(void)
     int length = ftell(in); // returns current file-position of stream from whence(SEEK_END);
 
     for(int i = 1; i <= length; ++i) { // read all bytes from stream(file 'in');
-        fseek(in, -i, SEEK_END); // sets the file position in stream from end to beginning of file 'in';
+        fseek(in, -i, SEEK_END); // sets the file position in stream from end(SEEK_END) to beginning(-i) of file 'in';
         putchar(fgetc(in)); // reads and outputs on dysplay the current char in file 'in';
     }
     
