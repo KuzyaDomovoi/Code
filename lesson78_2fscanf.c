4844f72fa9975b26e8d0e30023ac9495e5783f88
KuzyaDomovoi <vasilyshtanko@gmail.com>
2023-10-17 15:31:03 +0300

    C

--- lesson78_2fscanf.c
+++ lesson78_2fscanf.c
@@ -3,13 +3,12 @@
 // func fprintf() - performs a formatted read of data from the specified stream;
 
 
-enum {max_length = 1024}; // decl of constanta max_length that is the size of an array;
+enum {max_length = 1024}; // decl of constanta 'max_length' that is the size of an array;
 
 int main(void)
 {
-    
-   double rub_usd[max_length] = {0.0}; // the array with size max_length;
-   unsigned length = 0; // stores the number of files read;
+    double rub_usd[max_length] = {0.0}; // the array with the size 'max_length';
+    unsigned length = 0; // var 'length' stores the number of files read;
 
     FILE* fp = fopen("data_rubusd.csv", "r"); // open the file "data_rubusd.cvs" for reading;
     if(fp == NULL) {
@@ -17,7 +16,7 @@ int main(void)
         return 1;
     }
 
-    while(fscanf(fp, "%lf; ", &rub_usd[length]) == 1) // performs a formatted read of data from the specified stream while the func fscanf returns == 1(data read is correct);
+    while(fscanf(fp, "%lf; ", &rub_usd[length]) == 1) // performs a formatted read of data from the specified stream while the func fscanf returns == 1(while data read is correct);
         length++;
 
     fclose(fp);
