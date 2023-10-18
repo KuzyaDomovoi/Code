a6049d9c3d0ace5256f99cc1c4541bf10c688776
KuzyaDomovoi <vasilyshtanko@gmail.com>
2023-10-18 11:38:48 +0300

    C

--- lesson79_3setvbuf.c
+++ lesson79_3setvbuf.c
@@ -3,19 +3,20 @@
 // setvbuf() is the func that creats an own iobuffer while some file is opened by the func 'fopen()';
 /*
 int setvbuf(FILE * restrict stream, char * restrict buf, int mode, size_t size);
+
 Здесь указатель buf – это адрес области памяти для буфера (если равен NULL, 
 то буфер создается автоматически); mode – режим работы буфера; 
-size – размер буфера в байтах. Режимы определены следующими константами:
+size – размер буфера в байтах. 
 
+Режимы определены следующими константами:
     _IOFBF – полная буферизация (очистка после заполнения буфера);
     _IOLBF – построчная буферизация (очистка по символу переноса строки);
     _IONBF – выключение буферизации.
-    
+
 Функция setvbuf() должна вызываться после создания потока, но перед 
 использованием какой-либо операции записи/чтения данных из этого потока;
 */
-#include <stdio.h>
- 
+
 enum {buffer_size = 512, data_size = 100};
  
 int main(void)
@@ -28,7 +29,7 @@ int main(void)
         perror("data_rubusd_2.csv");
         return 1;
     }
- 
+
     if(setvbuf(fp, buffer, _IOFBF, buffer_size) != 0) {
         puts("Incorrect type or size of buffer");
         return 2;
