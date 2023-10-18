20e3db25b5fa489dc1f93dbb8c15b1bb2bf07cf8
KuzyaDomovoi <vasilyshtanko@gmail.com>
2023-10-18 11:25:09 +0300

    C

--- lesson79_2fflush.c
+++ lesson79_2fflush.c
@@ -4,25 +4,25 @@
 
 int main(void)
 {
-    int data[] = {1, 2, 3, 4, 5, 4, 3, 2, 1, -1};
-    int length = sizeof(data) / sizeof(*data);
+    int data[] = {1, 2, 3, 4, 5, 4, 3, 2, 1, -1}; // decl an array data;
+    int length = sizeof(data) / sizeof(*data); // definition the size of the array data;
  
-    FILE* fp = fopen("write_and_read.dat", "w");
-    FILE* in = fopen("write_and_read.dat", "r");
-    if(fp == NULL || in == NULL) {
-        perror("write_and_read.dat");
+    FILE* fp = fopen("write_and_read.dat", "w"); // creating and opening new file 'write_and_read.dat' with write-acces;
+    FILE* in = fopen("write_and_read.dat", "r"); // creating and opening the file with read-acces at the same time;
+    if(fp == NULL || in == NULL) { // checking if 'pf' or 'in' is NULL or they are opened successfull;
+        perror("write_and_read.dat"); // error output;
         return 1;
     }
  
-    for(int i = 0;i < length;++i)
-        fprintf(fp, "%d ", data[i]);
+    for(int i = 0; i < length; ++i) // iterate the array data;
+        fprintf(fp, "%d ", data[i]); // write formatted data from array data to the file 'fp';
  
-    fflush(fp);
+    fflush(fp); // transfer data read to file 'fp' and cleaning output stream after data write in file 'fp'; 
  
-    int value;
+    int value; // decl a new temporary var for loop 'for' to data read from file 'in';
     for(int i = 0; i < length; ++i)
-        if(fscanf(in, "%d ", &value) == 1)
-            printf("%d ",value);
+        if(fscanf(in, "%d ", &value) == 1) // conditions for data read from file 'in' in the var value;
+            printf("%d ",value); // output data from var value;
  
     fclose(fp);
     fclose(in);
