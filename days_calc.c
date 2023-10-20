3c8e04e18c405a1ef382516737d637666c6a0fac
KuzyaDomovoi <vasilyshtanko@gmail.com>
2023-10-20 14:50:26 +0300

    C

--- days_calc.c
+++ days_calc.c
@@ -5,7 +5,7 @@ int main(void)
 {
     time_t rawtime, then, now;
     struct tm* timeinfo;
-    int year, month, day, hour;
+    int year, month, day;
     int dif;
  
     printf("Enter year: "); 
@@ -14,24 +14,18 @@ int main(void)
     scanf("%d", &month);
     printf("Enter day: "); 
     scanf("%d", &day);
-    printf("Enter hour: "); 
-    scanf("%d", &hour);
  
     time(&rawtime);
     timeinfo = localtime(&rawtime);
     timeinfo->tm_year = year - 1900;
     timeinfo->tm_mon = month - 1;
     timeinfo->tm_mday = day;
-    timeinfo->tm_hour = hour;
  
     time(&now);
     then = mktime(timeinfo);
     dif = difftime(now, rawtime) + difftime(rawtime, then); // in seconds;
  
-    dif = dif / 3600; // in hours;
-    printf("\n %d hours have passed by since\n",dif);
- 
-    dif = dif / 24; // in days;
+    dif = dif / 3600 / 24; // in days;
     printf("\n %d days have passed by since\n",dif);
  
     getchar();
