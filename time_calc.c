bc7f582fbfa0f6e369979682f047295916892008
KuzyaDomovoi <vasilyshtanko@gmail.com>
2023-10-20 13:43:12 +0300

    C

--- time_calc.c
+++ time_calc.c
@@ -16,13 +16,13 @@ int main(void)
     unsigned int min_2 = (time_2 / 60) % 60;
     unsigned int sec_2 = time_2 % 60;
     
-    printf("Input the first data in the format: 'hhhh:mm:ss' for calculation: ");
-    while(scanf("%04d:%02d:%02d", &hour_1, &min_1, &sec_1) != 3) {
+    printf("Input the first data in the format: 'hour.min.sec' for calculation: ");
+    while(scanf("%d.%d.%d", &hour_1, &min_1, &sec_1) != 3) {
             printf("\nError input!\n");
             return 0;
         }
-    printf("Input the second data in the format: 'hhhh:mm:ss' for calculation: ");
-    while(scanf("%04d:%02d:%02d", &hour_2, &min_2, &sec_2) != 3) {
+    printf("Input the second data in the format: 'hour.min.sec' for calculation: ");
+    while(scanf("%d.%d.%d", &hour_2, &min_2, &sec_2) != 3) {
             printf("\nError input!\n");
             return 0; 
         }
@@ -45,7 +45,7 @@ int main(void)
         unsigned int hour_res_add = time_res_add / 3600;
         unsigned int min_res_add = (time_res_add / 60) % 60;
         unsigned int sec_res_add = time_res_add % 60;
-        printf("%04d:%02d:%02d + %04d:%02d:%02d = %04d:%02d:%02d\n", hour_1, min_1, sec_1, hour_2, min_2, sec_2, hour_res_add, min_res_add, sec_res_add);
+        printf("%d:%02d:%02d + %d:%02d:%02d = %d:%02d:%02d\n", hour_1, min_1, sec_1, hour_2, min_2, sec_2, hour_res_add, min_res_add, sec_res_add);
         return 0;
     case 2:
         printf("\nSubstraction:\n");
@@ -53,7 +53,7 @@ int main(void)
         unsigned int hour_res_sub = time_res_sub / 3600;
         unsigned int min_res_sub = (time_res_sub / 60) % 60;
         unsigned int sec_res_sub = time_res_sub % 60;
-        printf("%04d:%02d:%02d + %04d:%02d:%02d = %04d:%02d:%02d\n", hour_1, min_1, sec_1, hour_2, min_2, sec_2, hour_res_sub, min_res_sub, sec_res_sub);
+        printf("%d:%02d:%02d + %d:%02d:%02d = %d:%02d:%02d\n", hour_1, min_1, sec_1, hour_2, min_2, sec_2, hour_res_sub, min_res_sub, sec_res_sub);
         return 0;
     case 3:
         printf("\nExit.\n");
