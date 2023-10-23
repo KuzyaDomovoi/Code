f00010d0f3106e3ad72cc7f8d8c87cb36c8a3bee
KuzyaDomovoi <vasilyshtanko@gmail.com>
2023-10-23 14:38:09 +0300

    C

--- icalc.c
+++ icalc.c
@@ -42,42 +42,6 @@ int get_op(char op[]) { // the func reads operators or operands;
     }
 }
 
-/*
-typedef struct tag_obj {
-    int data; // stored data (objects) in stack;
-    struct tag_obj* next; // pointer next refers to the next following object in the stack;
-    } OBJ; // new datatype of struct tag_obj;
-
-OBJ* push(OBJ* top, int data) // the func push a new object in the stack
-{
-    OBJ* ptr = malloc(sizeof(OBJ)); // OBJ* ptr is a temporary pointer referring to allocated in HEAP the memory cells for a new object in stack;
-    ptr->data = data; // using the temporary pointer copying a value of current data;
-    printf("Added: %d\n", data); // for look up the currently added object to the stack;
-    ptr->next = top; // pointer 'next' copies the address, that is, refers to next (top) object added via func push of the stack currently;
-    return ptr; // via ptr is returned the address of a new added object in the stack;
-}
-
-OBJ* pop(OBJ* top) // the func pop pulls the current top object out the stack;
-{
-    if(top == NULL)
-        return top;
-    printf("Deleted: %d\n", top->data); // for look up the currently pulled object out the stack;
-    OBJ* ptr_next = top->next; // assigning the address of the next lower object to the current top object;
-    free(top); // makes free the allocated memory cells for the pulled current top object out the stack (deletes the current value of the top object);
-    return ptr_next; // returns the address of the next lower object or the value NULL when no more object exists in the stack already;
-}
-
-void show(const OBJ* top) // the func shows the all objects in the stack;
-{
-    const OBJ* current = top; // temporary pointer for iterating the objs of stack; initially it refers to the top object;
-    while (current != NULL) // loop while itarate the values until the current value will be equaled NULL;
-    {
-        printf("%d\n", current->data); // output the current object in the stack;
-        current = current->next; // go to the next lower object in the stack or to the value NULL (than the loop will be end);
-    }
-}
-*/
-
 int steak[STEAKSIZE]; // the array for the steak creating with a size 'STEAKSIZE 100';
 int free_si = 0; // checking a free memory cell in the steak;
 
@@ -98,13 +62,6 @@ int get(void) { // returns the last saved number from the steak;
 int main(void)
 {
     char oper[SIZE]; // the string for operands or operators and is an argument for the func get_op();
-    
-    // code for checking the correct operation of the funcs added to the file 'functions.c':
-    /*int result = get_op(op); // assigning a number or an arithmetic sign to the var 'result' wich are returned by the func get_op();
-    printf("result of get_op = %d\n", result);
-    printf("op[] = %s\n", op);
-    printf("(int) result: %d", str_to_int(op));*/
-
     int op; // the var for the first operand or for an operator;
     int op2; // the var for the second operand;
     while((op = get_op(oper)) != 'x') { // if the char 'x' is entered the loop will end;
