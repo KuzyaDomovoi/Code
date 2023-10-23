5074a56040e53adb755a5e4713bcaaf55390463e
KuzyaDomovoi <vasilyshtanko@gmail.com>
2023-10-23 14:35:30 +0300

    C

--- functions.c
+++ functions.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 
 #define NUMBER 1000
-//#define STEAKSIZE 100
+#define STEAKSIZE 100
 
 int str_to_int(char str[]){ // the func converts the string to a number;
     int num = 0;
@@ -40,7 +40,7 @@ int get_op(char op[]) { // the func reads operators or operands;
         return NUMBER; // return the number in thr string op[];
     }
 }
-/*
+
 int steak[STEAKSIZE]; // the array for the steak creating with a size 'STEAKSIZE 100';
 int free_si = 0; // checking a free memory cell in the steak;
 
@@ -56,38 +56,4 @@ int gett(void) { // returns the last saved number from the steak;
         return steak[--free_si]; // ('--free_si' is a previous index in steak because the index free_si itself points to the last memory cell in the steak) is returned the last saved number from the steak; 
     else
         printf("Error! Steak is empty!");
-}
-*/
-typedef struct tag_obj {
-    int data; // stored data (objects) in stack;
-    struct tag_obj* next; // pointer next refers to the next following object in the stack;
-    } OBJ; // new datatype of struct tag_obj;
-
-OBJ* push(OBJ* top, int data) // the func push a new object in the stack
-{
-    OBJ* ptr = malloc(sizeof(OBJ)); // OBJ* ptr is a temporary pointer referring to allocated in HEAP the memory cells for a new object in stack;
-    ptr->data = data; // using the temporary pointer copying a value of current data;
-    printf("Added: %d\n", data); // for look up the currently added object to the stack;
-    ptr->next = top; // pointer 'next' copies the address, that is, refers to next (top) object added via func push of the stack currently;
-    return ptr; // via ptr is returned the address of a new added object in the stack;
-}
-
-OBJ* pop(OBJ* top) // the func pop pulls the current top object out the stack;
-{
-    if(top == NULL)
-        return top;
-    printf("Deleted: %d\n", top->data); // for look up the currently pulled object out the stack;
-    OBJ* ptr_next = top->next; // assigning the address of the next lower object to the current top object;
-    free(top); // makes free the allocated memory cells for the pulled current top object out the stack (deletes the current value of the top object);
-    return ptr_next; // returns the address of the next lower object or the value NULL when no more object exists in the stack already;
-}
-
-void show(const OBJ* top) // the func shows the all objects in the stack;
-{
-    const OBJ* current = top; // temporary pointer for iterating the objs of stack; initially it refers to the top object;
-    while (current != NULL) // loop while itarate the values until the current value will be equaled NULL;
-    {
-        printf("%d\n", current->data); // output the current object in the stack;
-        current = current->next; // go to the next lower object in the stack or to the value NULL (than the loop will be end);
-    }
 }
