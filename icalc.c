725a6059da2a45d6392074ee998f90e86282a041
KuzyaDomovoi <vasilyshtanko@gmail.com>
2023-10-24 06:48:16 +0300

    C

--- icalc.c
+++ icalc.c
@@ -6,7 +6,7 @@
 #define STEAKSIZE 100
 #define SIZE 100
 
-int str_to_int(char str[]){ // the func converts the string to a number;
+int str_to_int(char str[]) { // the func converts the string to a number;
     int num = 0;
     int i;
     for(int i = 0; str[i] != '\0'; i++)
@@ -14,30 +14,30 @@ int str_to_int(char str[]){ // the func converts the string to a number;
     return num;
 }
 
-int get_op(char op[]) { // the func reads operators or operands;
-    int c = 0; // var for the symbol storing;
+int get_symb(char symb[]) { // the func reads operators or operands;
+    int ch = 0; // var for the symbol storing;
     static int remember = 0; // the var 'remember' is set to '0' initially and then stores a current value entered by the func get_op();
     int i = 0; // the counter for the string op[];
     if(remember != 0) { // checking if the var 'remember' receives some value (not 0) then
-        c = remember; // to the var 'c' is assigned a value of the var 'remember';
+        ch = remember; // to the var 'ch' is assigned a value of the var 'remember';
         remember = 0; // and then the var 'remember' will be reset to 0 again;
     }else
-        c = getchar(); // if the var 'remember' is '0' then to the var 'c' is assigned a symbol read by the func getchar();
+        ch = getchar(); // if the var 'remember' is '0' then to the var 'ch' is assigned a symbol read by the func getchar();
 
-    while(c == ' ' || c == '\t') { // the loop skips the indentation or tabulation at beginn incase the var 'c' is a spacesymbol;
-        c = getchar();
+    while(ch == ' ' || ch == '\t') { // the loop skips the indentation or tabulation at beginn incase the var 'ch' is a spacesymbol;
+        ch = getchar();
     }
-    if(!isdigit(c)) // if the var 'c' is not a digit then 
-        return c; // is returned a symbol or the '\n' from var 'c';
+    if(!isdigit(ch)) // if the var 'ch' is not a digit then 
+        return ch; // is returned a symbol or the '\n' from var 'ch';
     
-    if(isdigit(c)) { // check if var 'c' is a number;
-        op[i] = c; // write the current number to str:
-        while(isdigit(op[++i] = (c = getchar()))) // while the each next var 'c' is a number the loop is continue and the number is writted to the string op[];
+    if(isdigit(ch)) { // check if var 'ch' is a number;
+        symb[i] = ch; // write the current number to str:
+        while(isdigit(symb[++i] = (ch = getchar()))) // while the each next var 'ch' is a number the loop is continue and the number is writted to the string symb[];
             ;
-        remember = c; // a value of the var 'c' is assigned to the var 'remember';
-        op[i] = '\0'; // and 'no terminated sign' is writted at the end; 
+        remember = ch; // a value of the var 'ch' is assigned to the var 'remember';
+        symb[i] = '\0'; // and 'no terminated sign' is writted at the end; 
         
-        return NUMBER; // return the number in thr string op[];
+        return NUMBER; // return the number in thr string symb[];
     }
 }
 
@@ -60,15 +60,15 @@ int get(void) { // returns the last saved number from the steak;
 
 int main(void)
 {
-    char oper[SIZE]; // the string for operators and is an argument for the func get_op();
+    char oper[SIZE]; // the string for operators and is an argument for the func get_symb();
     int op, op2; // the vars for the operands;
-    while((op = get_op(oper)) != 'x') { // if 'x' - exit program;
+    while((op = get_symb(oper)) != 'x') { // if 'x' - exit program;
         switch(op) { // cheking the var op;
         case NUMBER: // if 'op' is a number
             push(str_to_int(oper)); // then push the number in steak;
             break;
         case '+':
-            if((op2 = get_op(oper)) == NUMBER) // if the func get_op() returns a number
+            if((op2 = get_symb(oper)) == NUMBER) // if the func get_symb() returns a number
                 op2 = str_to_int(oper); // to the var op2 is assigned the second operand(number) from the func str_to_int();
             else {
                 printf("There must be a number\n");
@@ -77,7 +77,7 @@ int main(void)
             push(get() + op2); // the operands will be calculated and pushed in the steak initially;
             break;
         case '-':
-            if((op2 = get_op(oper)) == NUMBER) 
+            if((op2 = get_symb(oper)) == NUMBER) 
                 op2 = str_to_int(oper); 
             else {
                 printf("There must be a number\n");
@@ -86,7 +86,7 @@ int main(void)
             push(get() - op2);
             break;
         case '*':
-            if((op2 = get_op(oper)) == NUMBER) 
+            if((op2 = get_symb(oper)) == NUMBER) 
                 op2 = str_to_int(oper); 
             else {
                 printf("There must be a number\n");
@@ -95,7 +95,7 @@ int main(void)
             push(get() * op2);
             break;
         case '/':
-            if((op2 = get_op(oper)) == NUMBER) 
+            if((op2 = get_symb(oper)) == NUMBER) 
                 op2 = str_to_int(oper);
             else {
                 printf("There must be a number\n");
@@ -104,7 +104,7 @@ int main(void)
             push(get() / op2);
             break;
         default:    
-            printf("Unknown an operator or an operand\n");
+            printf("Unknown operator or operand\n");
             exit(1);
             break;
         }
