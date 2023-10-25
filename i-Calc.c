1bbc4117a3f0249d96e85096fd18560d09385e32
KuzyaDomovoi <vasilyshtanko@gmail.com>
2023-10-25 12:27:59 +0300

    C

--- i-Calc.c
+++ i-Calc.c
@@ -78,7 +78,7 @@ int main(void)
         }
     case 2:
         printf("Input a number for calculation: ");
-        while(scanf("%lf %lf", &x, &y) != 1) {
+        while(scanf("%d %d", &x, &y) != 1) {
             printf("\nError input!\n");
             return 0;
         }
