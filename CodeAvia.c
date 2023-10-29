6be0bdc59464665cefe4ebf9b1e083e7e9f09a7f
KuzyaDomovoi <vasilyshtanko@gmail.com>
2023-10-29 15:36:04 +0300

    С

--- CodeAvia.c
+++ CodeAvia.c
@@ -107,12 +107,12 @@ int main(void)
         rangcruise = fucons_cruise / q;
         timecruise = rangcruise / cruisspeed;
         flrange = (midaverage_climspeed * climtime) + ((cruisspeed / 3.6) * timecruise / 3.6) + (descspeed * desctime);
-        flduration = (climtime / 60) + timecruise + (desctime / 60);
+        flduration = (climtime * 60) + (timecruise * 3600) + (desctime * 60);
         int flduration_h = (int)flduration / 3600;
         int flduration_m = (int)flduration % 3600 / 60;
         
         printf("\nРасполагаемый запас топлива = %.2f\n", fucons_cruise);
-        printf("Дальность полета, км = %.f\nПродолжительность полета, ч = %d.%02d\n", flrange, flduration_h, flduration_m);
+        printf("Дальность полета, км = %.f\nПродолжительность полета, ч = %d ч %02d мин\n", flrange, flduration_h, flduration_m);
         return 0;
     case 2:
         printf("\n   1. Определение радиуса разворота по углу крена и скорости разворота\n"
