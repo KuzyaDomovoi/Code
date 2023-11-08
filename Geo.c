b995845134efcd6b1c9c79cb30c393f6ccf07ec1
KuzyaDomovoi <vasilyshtanko@gmail.com>
2023-11-08 10:17:52 +0300

    C

--- Geo.c
+++ Geo.c
@@ -5,6 +5,7 @@
 struct geo_nlat {
     unsigned nlat;
     unsigned sec;
+    unsigned msec;
     unsigned grad;
     unsigned min;
 } nlat;
@@ -12,30 +13,31 @@ struct geo_nlat {
 struct geo_elon {
     unsigned elon;
     unsigned sec;
+    unsigned msec;
     unsigned grad;
     unsigned min;
 } elon;
 
 int main(void)
 {
-    printf("Input coordinats of northern latitude in format gg.mm.ss: ");
-    while(scanf("%d.%d.%d", &nlat.grad, &nlat.min, &nlat.sec) != 3) {
+    printf("Input coordinats of northern latitude in format gg.mm.ss,ms: ");
+    while(scanf("%d.%d.%d,%d", &nlat.grad, &nlat.min, &nlat.sec, &nlat.msec) != 4) {
         printf("Incorrect input!\n");
         return 0;
     } 
     
-    printf("Input coordinats of eastern longitude in format gg.mm.ss: ");
-    while(scanf("%d.%d.%d", &elon.grad, &elon.min, &elon.sec) != 3) {
+    printf("Input coordinats of eastern longitude in format gg.mm.ss,ms: ");
+    while(scanf("%d.%d.%d", &elon.grad, &elon.min, &elon.sec, &elon.msec) != 4) {
         printf("Incorrect input!\n");
         return 0;
     }
-    //geo_nlat.nlat = geo_nlat.grad * 3600 + geo_nlat.min * 60 + geo_nlat.sec;
+    //nlat.nlat = nlat.grad * 3600 + nlat.min * 60 + nlat.sec;
 
-    //geo_elon.elon = geo_elon.grad * 3600 + geo_elon.min * 60 * geo_elon.sec;
+    //elon.elon = elon.grad * 3600 + elon.min * 60 * elon.sec;
 
 
-    printf("N: %03d:%02d:%02d\n", nlat.grad, nlat.min, nlat.sec);
-    printf("E: %03d:%02d:%02d\n", elon.grad, elon.min, elon.sec);
+    printf("N: %03d:%02d:%02d,%02d\n", nlat.grad, nlat.min, nlat.sec, nlat.msec);
+    printf("E: %03d:%02d:%02d,%02d\n", elon.grad, elon.min, elon.sec, elon.msec);
 
     return 0;
 }
