4f49cd772793447da7e0141c125c1f5a5ceb92a5
KuzyaDomovoi <vasilyshtanko@gmail.com>
2023-11-08 10:30:06 +0300

    C

--- Geo.c
+++ Geo.c
@@ -21,19 +21,19 @@ struct geo_elon {
 int main(void)
 {
     printf("Input coordinats of northern latitude in format gg.mm.ss,ms: ");
-    while(scanf("%d.%d.%d,%d", &nlat.grad, &nlat.min, &nlat.sec, &nlat.msec) < 3 && scanf("%d.%d.%d,%d", &nlat.grad, &nlat.min, &nlat.sec, &nlat.msec) > 4) {
+    while(scanf("%d %d %d.%d", &nlat.grad, &nlat.min, &nlat.sec, &nlat.msec) < 3 && scanf("%d.%d.%d,%d", &nlat.grad, &nlat.min, &nlat.sec, &nlat.msec) > 4) {
         printf("Incorrect input!\n");
         return 0;
     } 
     
     printf("Input coordinats of eastern longitude in format gg.mm.ss,ms: ");
-    while(scanf("%d.%d.%d,%d", &elon.grad, &elon.min, &elon.sec, &elon.msec) < 3 && scanf("%d.%d.%d,%d", &elon.grad, &elon.min, &elon.sec, &elon.msec) > 4) {
+    while(scanf("%d %d %d.%d", &elon.grad, &elon.min, &elon.sec, &elon.msec) < 3 && scanf("%d.%d.%d,%d", &elon.grad, &elon.min, &elon.sec, &elon.msec) > 4) {
         printf("Incorrect input!\n");
         return 0;
     }
 
-    printf("N: %03d:%02d:%02d,%02d\n", nlat.grad, nlat.min, nlat.sec, nlat.msec);
-    printf("E: %03d:%02d:%02d,%02d\n", elon.grad, elon.min, elon.sec, elon.msec);
+    printf("N: %03d %02d %02d.%02d\n", nlat.grad, nlat.min, nlat.sec, nlat.msec);
+    printf("E: %03d %02d %02d.%02d\n", elon.grad, elon.min, elon.sec, elon.msec);
 
     return 0;
 }
