8d22c30b76c1aa2cfe4b865b2a72c8867a63bd24
KuzyaDomovoi <vasilyshtanko@gmail.com>
2023-11-09 11:50:28 +0300

    C

--- Geo.c
+++ Geo.c
@@ -10,13 +10,13 @@ struct geo_nlat {
     unsigned min;
 } nlat;
 
-struct geo_elon {
-    unsigned elon;
+struct geo_elng {
+    unsigned elng;
     unsigned sec;
     unsigned msec;
     unsigned grad;
     unsigned min;
-} elon;
+} elng;
 
 int main(void)
 {
@@ -26,12 +26,12 @@ int main(void)
         return 0;
     } 
     printf("Input coordinats of eastern longitude in format gg.mm.ss,ms: ");
-    while(scanf("%d %d %d.%d", &elon.grad, &elon.min, &elon.sec, &elon.msec) < 3 && scanf("%d.%d.%d,%d", &elon.grad, &elon.min, &elon.sec, &elon.msec) > 4) {
+    while(scanf("%d %d %d.%d", &elng.grad, &elng.min, &elng.sec, &elng.msec) < 3 && scanf("%d.%d.%d,%d", &elng.grad, &elng.min, &elng.sec, &elng.msec) > 4) {
         printf("Incorrect input!\n");
         return 0;
     }
     printf("N: %03d %02d %02d.%02d\n", nlat.grad, nlat.min, nlat.sec, nlat.msec);
-    printf("E: %03d %02d %02d.%02d\n", elon.grad, elon.min, elon.sec, elon.msec);
+    printf("E: %03d %02d %02d.%02d\n", elng.grad, elng.min, elng.sec, elng.msec);
 
     return 0;
 }
