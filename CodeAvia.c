7940e1e9bdf33fa3bcc2a58cb552d3589c968fd4
KuzyaDomovoi <vasilyshtanko@gmail.com>
2023-11-12 11:19:34 +0300

    C

--- CodeAvia.c
+++ CodeAvia.c
@@ -229,8 +229,7 @@ int main(void)
     case 2:
         printf("\nРасчет дальности между двумя точками заданными географическими координатами\n");
         printf("\n   Введи через пробел координаты WGS-84 для широты первой точки: ");
-        while(scanf("%d %d %d.%d", &lat_1.grad, &lat_1.min, &lat_1.sec, &lat_1.msec) < 3 && 
-              scanf("%d %d %d.%d", &lat_1.grad, &lat_1.min, &lat_1.sec, &lat_1.msec) > 4) {
+        while(scanf("%d %d %d.%d", &lat_1.grad, &lat_1.min, &lat_1.sec, &lat_1.msec) <= 4) {
             printf("Incorrect input!\n");
             return 0;
         }
@@ -243,8 +242,7 @@ int main(void)
             return 0;            
         }
         printf("   Введи через пробел координаты WGS-84 для долготы первой точки: ");
-        while(scanf("%d %d %d.%d", &lng_1.grad, &lng_1.min, &lng_1.sec, &lng_1.msec) < 3 && 
-              scanf("%d %d %d.%d", &lng_1.grad, &lng_1.min, &lng_1.sec, &lng_1.msec) > 4) {
+        while(scanf("%d %d %d.%d", &lng_1.grad, &lng_1.min, &lng_1.sec, &lng_1.msec) <= 4) {
             printf("Incorrect input!\n");
             return 0;
         }
@@ -257,8 +255,7 @@ int main(void)
             return 0;            
         }
         printf("\n   Введи через пробел координаты WGS-84 для широты второй точки: ");
-        while(scanf("%d %d %d.%d", &lat_2.grad, &lat_2.min, &lat_2.sec, &lat_2.msec) < 3 && 
-              scanf("%d %d %d.%d", &lat_2.grad, &lat_2.min, &lat_2.sec, &lat_2.msec) > 4) {
+        while(scanf("%d %d %d.%d", &lat_2.grad, &lat_2.min, &lat_2.sec, &lat_2.msec) <= 4) {
             printf("Incorrect input!\n");
             return 0;
         }
@@ -271,8 +268,7 @@ int main(void)
             return 0;            
         }        
         printf("   Введи через пробел координаты WGS-84 для долготы второй точки: ");
-        while(scanf("%d %d %d.%d", &lng_2.grad, &lng_2.min, &lng_2.sec, &lng_2.msec) < 3 && 
-              scanf("%d %d %d.%d", &lng_2.grad, &lng_2.min, &lng_2.sec, &lng_2.msec) > 4) {
+        while(scanf("%d %d %d.%d", &lng_2.grad, &lng_2.min, &lng_2.sec, &lng_2.msec) <= 4) {
             printf("Incorrect input!\n");
             return 0;
         }
