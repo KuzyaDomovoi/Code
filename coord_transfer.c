afde31bcc5878aae52fff5ba7a75ee23595567a5
KuzyaDomovoi <vasilyshtanko@gmail.com>
2023-11-15 17:24:59 +0300

    C

--- coord_transfer.c
+++ coord_transfer.c
@@ -1,35 +1,29 @@
 #include <stdio.h>
 #include <math.h>
 
-void coord_transfer(float degrees, int result[3]) {
+void coord_transfer(double degrees, int result[2], float result2[1]) {
     int dd = trunc(degrees);
     int mm = trunc((degrees - dd) * 60);
-    int ss = trunc(((degrees - dd) * 60 - mm) * 60);
+    float ss = ((degrees - dd) * 60 - mm) * 60;
     
     result[0] = dd;
     result[1] = mm;
-    result[2] = ss;
+    result2[0] = ss;
 }
 
 int main(void)
 {
-    float degrees;
+    double degrees;
     printf("dd.ddddd: ");
-    if(scanf("%f", &degrees) != 1)
+    if(scanf("%lf", &degrees) != 1) {
         printf("incorrect input!\n");
         return 0;
+    }
+    int result[2];
+    float result2[1];
+    coord_transfer(degrees, result, result2);
 
-    //int result[3];
-    //coord_transfer(degrees, result);
-
-    int dd = trunc(degrees);
-    int mm = trunc((degrees - dd) * 60);
-    int ss = trunc(((degrees - dd) * 60 - mm) * 60);
-
-    printf("%d° %d' %d''\n", dd, mm, ss);
-    printf("%d°\n", dd);
-
-    //printf("%d° %d' %d''\n", result[0], result[1], result[2]);
+    printf("%d° %d' %.2f''\n", result[0], result[1], result2[0]);
 
     return 0;
 }
