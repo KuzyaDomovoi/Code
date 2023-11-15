c1ee1bc59ec47b1b92408ee2ff34a62b9430733d
KuzyaDomovoi <vasilyshtanko@gmail.com>
2023-11-15 17:38:14 +0300

    C

--- coord_transfer.c
+++ coord_transfer.c
@@ -24,20 +24,23 @@ int main(void)
     double lat_deg;
     double lng_deg;
     printf("lat dd.ddddd: ");
-    if(scanf("%lf %lf", &lat_deg, &lng_deg) != 1) {
+    if(scanf("%lf", &lat_deg) != 1) {
         printf("incorrect input!\n");
         return 0;
     }
     printf("lng dd.ddddd: ");
-    if(scanf("%lf %lf", &lat_deg, &lng_deg) != 1) {
+    if(scanf("%lf", &lng_deg) != 1) {
         printf("incorrect input!\n");
         return 0;
     }
-    int res[2];
-    float res2[1];
-    coord_transfer(lat_deg, res, res2);
+    int lat_res[2];
+    float lat_res2[1];
+    int lng_res[2];
+    float lng_res2[1];   
+    coord_transfer(lat_deg, lng_deg, lat_res, lat_res2, lng_res, lng_res2);
 
-    printf("%d° %d' %.2f''\n", res[0], res[1], res2[0]);
+    printf("lat  %02d° %d' %.2f''\nlng %03d° %d' %.2f''\n", 
+            lat_res[0], lat_res[1], lat_res2[0], lng_res[0], lng_res[1], lng_res2[0]);
 
     return 0;
 }
