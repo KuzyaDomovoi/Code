85ecd24922fe4bca10dc2ba9d93775374dfb8ea9
KuzyaDomovoi <vasilyshtanko@gmail.com>
2023-11-18 15:56:25 +0300

    C

--- time_calc2.c
+++ time_calc2.c
@@ -3,6 +3,7 @@
 #include <stdbool.h>
 
 struct time_calc {
+    int time;
     int hour;
     int min;
     int sec;
@@ -17,12 +18,14 @@ bool input_time(int h, int m, int s, int input) {
     } else return false;
 }
 
-void transfer_time(int h, int m, int s, int transfer[1]) {
-    transfer[0] = h * 3600 + m * 60 + s;
+int transfer_time(int h, int m, int s) {
+    int transfer = h * 3600 + m * 60 + s;
+    return transfer;
 }
 
-void calc_time(int a, int b, int sum[1]) {
-    sum[0] = a + b;
+int calc_time(int a, int b) {
+    int sum = a + b;
+    return sum;
 }
 
 void res_transfer_time(int t, int time[3]) {
@@ -41,23 +44,21 @@ int main(void)
     if(input_time(time_1.hour, time_1.min, time_1.sec, input) != 0) {
         return 0;
     }
-    int transfer_1[1];
-    transfer_time(time_1.hour, time_1.min, time_1.sec, transfer_1);
-
     printf("\nInput time_2 in format: 'hour.min.sec': ");
     input = scanf("%d.%d.%d", &time_2.hour, &time_2.min, &time_2.sec);
     if(input_time(time_2.hour, time_2.min, time_2.sec, input) != 0) {
         return 0;
     }
-    int transfer_2[1];
-    transfer_time(time_2.hour, time_2.min, time_2.sec, transfer_2);
+    int transfer_1 = transfer_time(time_1.hour, time_1.min, time_1.sec);
+    int transfer_2 = transfer_time(time_2.hour, time_2.min, time_2.sec);
 
-    int sum[1];
-    calc_time(transfer_1, transfer_2, sum);
-
+    int sum = calc_time(transfer_1, transfer_2);
+    
     int time[3];
     res_transfer_time(sum, time);
 
+    printf("%d\n", time_1.time);
+    printf("%d\n", time_2.time);
     printf("\ntime = %d:%02d:%02d\n", time[0], time[1], time[2]);
     
     return 0;     
