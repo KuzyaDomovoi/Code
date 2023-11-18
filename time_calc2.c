5e67b6bdb74901e1381a5aa4a7a8763a9739e391
KuzyaDomovoi <vasilyshtanko@gmail.com>
2023-11-18 21:46:19 +0300

    C

--- time_calc2.c
+++ time_calc2.c
@@ -12,10 +12,10 @@ struct time_calc {
 struct time_calc time_2;
 
 bool input_time(int h, int m, int s, int input) {
-    if(input != 3) {
+    while(input != 3) {
         printf("\nIt should be entered hh.mm.ss\n");
         return true;
-    } else return false;
+    } return false;
 }
 
 int input_transfer_time(int h, int m, int s) {
