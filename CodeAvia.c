2f7888bfb5069ba08a1e9a5cbdc7b5d5d1bb1e83
KuzyaDomovoi <vasilyshtanko@gmail.com>
2023-11-19 21:40:24 +0300

    C

--- CodeAvia.c
+++ CodeAvia.c
@@ -35,17 +35,17 @@ void coord_transfer_deg(double deg, int result[2], double result2[1]) {
     result2[0] = fabsf(ss);
 }
 
-void coord_transfer_wgs84(int deg, int min, int sec, int msec, double result[2]) {
-    double lat, lng;
+void coord_transfer_wgs84(float deg, float min, float sec, float result[2]) {
+    float lat, lng;
     if(lat_1.deg < 0) {
-        lat = -1 * (abs(lat_1.deg) + lat_1.min / 60.0 + lat_1.sec / 3600.0 + lat_1.msec / 3600.0 / 60.0);
+        lat = -1 * (abs(lat_1.deg) + (lat_1.min * 60.0 + lat_1.sec) / 3600.0);
     } else
-        lat = lat_1.deg + lat_1.min / 60.0 + lat_1.sec / 3600.0 + lat_1.msec / 3600.0 / 60;
+        lat = lat_1.deg + (lat_1.min * 60.0 + lat_1.sec) / 3600.0;
 
     if(lng_1.deg < 0) {
-        lng =  -1 * (abs(lng_1.deg) + lng_1.min / 60.0 + lng_1.sec / 3600.0 + lng_1.msec / 3600.0 / 60.0);
+        lng =  -1 * (abs(lng_1.deg) + (lng_1.min * 60.0 + lng_1.sec) / 3600.0);
     } else
-    lng = lng_1.deg + lng_1.min / 60.0 + lng_1.sec / 3600.0 + lng_1.msec / 3600.0 / 60.0;
+    lng = lng_1.deg + (lng_1.min * 60.0 + lng_1.sec) / 3600.0;
 
     result[0] = lat;
     result[1] = lng;
@@ -112,8 +112,8 @@ bool range2(int x, int a, int y) {
     return false;
 }
 
-bool input_verif_lat(int a, int b, int c, int d, int res) {
-    if(res != 4) {
+bool input_verif_lat(int a, int b, int c, int res) {
+    if(res != 3) {
         printf("\nIncorrect input! It should be entered 4 values!\n");
         return true;
     }
@@ -121,22 +121,18 @@ bool input_verif_lat(int a, int b, int c, int d, int res) {
         printf("\nIncorrect input! Range of latitude is -90...90\n");
         return true;
     }
-    if(range2(0, b, 59) || range2(0, c, 59)) {
-        printf("\nIncorrect input! Range of min and sec is 0...59\n");
+    if(range2(0, b, 59) || range2(0, c, 59.99)) {
+        printf("\nIncorrect input! Range of min and sec is 0...59.99\n");
         return true;
     }
-    if(range2(0, d, 99)) {
-        printf("\nIncorrect input! Range of msec is 0...99\n");
-        return true;
-    }
-    if(range2(-89, a, 89) && (b > 0 || c > 0 || d > 0)) {
-        printf("\nIncorrect input! If the latitude has value -90 or 90, the values of min, sec and msec should be 00 only\n");
+    if(range2(-89, a, 89) && (b > 0 || c > 0)) {
+        printf("\nIncorrect input! If the latitude has value -90 or 90, the values of min and sec should be 00 only\n");
         return true;
     } else return false;
 }
 
-bool input_verif_lng(int a, int b, int c, int d, int res) {
-    if(res != 4) {
+bool input_verif_lng(int a, int b, int c, int res) {
+    if(res != 3) {
         printf("\nIncorrect input! It should be entered 4 values!\n");
         return true;
     }
@@ -144,16 +140,12 @@ bool input_verif_lng(int a, int b, int c, int d, int res) {
         printf("\nIncorrect input! Range of latitude is -180...180\n");
         return true;
     }
-    if(range2(0, b, 59) || range2(0, c, 59)) {
-        printf("\nIncorrect input! Range of min and sec is 0...59\n");
+    if(range2(0, b, 59) || range2(0, c, 59.99)) {
+        printf("\nIncorrect input! Range of min and sec is 0...59.99\n");
         return true;
     }
-    if(range2(0, d, 99)) {
-        printf("\nIncorrect input! Range of msec is 0...99\n");
-        return true;
-    }
-    if(range2(-179, a, 179) && (b > 0 || c > 0 || d > 0)) {
-        printf("\nIncorrect input! If the longitude has value -180 or 180, the values of min, sec and msec should be 00 only\n");
+    if(range2(-179, a, 179) && (b > 0 || c > 0)) {
+        printf("\nIncorrect input! If the longitude has value -180 or 180, the values of min and sec should be 00 only\n");
         return true;
     } else return false;
 }
@@ -164,9 +156,9 @@ int main(void)
     int res = 0;
     double lat1, lat2, lng1, lng2;
     int lat_res[2];
-    double lat_res2[1];
+    float lat_res2[1];
     int lng_res[2];
-    double lng_res2[1];
+    float lng_res2[1];
 
     printf("\n1. Расчет дальности и продолжительности полета\n"
              "2. Расчет расстояния между двумя точками по их координатам\n"
@@ -285,45 +277,45 @@ int main(void)
     case 2:
         printf("\nРасчет расстояния между двумя точками по их координатам\n");
         printf("\n   Введи координаты гг мм сс.мс WGS-84 широты первой точки: ");
-        res = scanf("%d %d %d.%d", &lat_1.deg, &lat_1.min, &lat_1.sec, &lat_1.msec);
-        if(input_verif_lat(lat_1.deg, lat_1.min, lat_1.sec, lat_1.msec, res) != 0)
+        res = scanf("%d %d %f", &lat_1.deg, &lat_1.min, &lat_1.sec);
+        if(input_verif_lat(lat_1.deg, lat_1.min, lat_1.sec, res) != 0)
             return 0;
         printf("   Введи координаты ггг мм сс.мс WGS-84 долготы первой точки: ");
-        res = scanf("%d %d %d.%d", &lng_1.deg, &lng_1.min, &lng_1.sec, &lng_1.msec);
-        if(input_verif_lng(lng_1.deg, lng_1.min, lng_1.sec, lng_1.msec, res) !=0)
+        res = scanf("%d %d %f", &lng_1.deg, &lng_1.min, &lng_1.sec);
+        if(input_verif_lng(lng_1.deg, lng_1.min, lng_1.sec, res) != 0)
             return 0;
         printf("\n   Введи координаты гг мм сс.мс WGS-84 широты второй точки: ");
-        res = scanf("%d %d %d.%d", &lat_2.deg, &lat_2.min, &lat_2.sec, &lat_2.msec);
-        if(input_verif_lat(lat_2.deg, lat_2.min, lat_2.sec, lat_2.msec, res) != 0)
+        res = scanf("%d %d %f", &lat_2.deg, &lat_2.min, &lat_2.sec);
+        if(input_verif_lat(lat_2.deg, lat_2.min, lat_2.sec, res) != 0)
             return 0;
         printf("   Введи координаты ггг мм сс.мс WGS-84 долготы второй точки: ");
-        res = scanf("%d %d %d.%d", &lng_2.deg, &lng_2.min, &lng_2.sec, &lng_2.msec);
-        if(input_verif_lng(lng_2.deg, lng_2.min, lng_2.sec, lng_2.msec, res) != 0)
+        res = scanf("%d %d %f", &lng_2.deg, &lng_2.min, &lng_2.sec);
+        if(input_verif_lng(lng_2.deg, lng_2.min, lng_2.sec res) != 0)
             return 0;
 
         if(lat_1.deg < 0) {
-            lat1 = -1 * (abs(lat_1.deg) + lat_1.min / 60.0 + lat_1.sec / 3600.0 + lat_1.msec / 3600.0 / 60.0);
+            lat1 = -1 * (abs(lat_1.deg) + (lat_1.min * 60.0 + lat_1.sec) / 3600.0);
         } else
-            lat1 = lat_1.deg + lat_1.min / 60.0 + lat_1.sec / 3600.0 + lat_1.msec / 3600.0 / 60.0;
+            lat1 = lat_1.deg + (lat_1.min * 60.0 + lat_1.sec) / 3600.0;
         if(lng_1.deg < 0) {
-            lng1 = -1 * abs(lng_1.deg) + lng_1.min / 60.0 + lng_1.sec / 3600.0 + lng_1.msec / 3600.0 / 60.0;
+            lng1 = -1 * abs(lng_1.deg) + (lng_1.min * 60.0) + lng_1.sec / 3600.0;
         } else
-            lng1 = lng_1.deg + lng_1.min / 60.0 + lng_1.sec / 3600.0 + lng_1.msec / 3600.0 / 60.0;
+            lng1 = lng_1.deg + (lng_1.min * 60.0 + lng_1.sec) / 3600.0;
         if(lat_2.deg < 0) {
-            lat2 = -1 * abs(lat_2.deg )+ lat_2.min / 60.0 + lat_2.sec / 3600.0 + lat_2.msec / 3600.0 / 60.0;
+            lat2 = -1 * abs(lat_2.deg) + (lat_2.min * 60.0 + lat_2.sec) / 3600.0;
         } else
-            lat2 = lat_2.deg + lat_2.min / 60.0 + lat_2.sec / 3600.0 + lat_2.msec / 3600.0 / 60.0;
+            lat2 = lat_2.deg + (lat_2.min * 60.0 + lat_2.sec) / 3600.0;
         if(lng_2.deg < 0) {
-            lng2 = -1 * abs(lng_2.deg) + lng_2.min / 60.0 + lng_2.sec / 3600.0 + lng_2.msec / 3600.0 / 60.0;
+            lng2 = -1 * abs(lng_2.deg) + (lng_2.min * 60.0 + lng_2.sec) / 3600.0;
         } else
-            lng2 = lng_2.deg + lng_2.min / 60.0 + lng_2.sec / 3600.0 + lng_2.msec / 3600.0 / 60.0;
+            lng2 = lng_2.deg + (lng_2.min * 60.0 + lng_2.sec) / 3600.0;
 
         double result[3];
         calcfldist_bear(lat1, lng1, lat2, lng2, result);
-        printf("\nПервая точка: lat %4d° %02d' %02d.%02d''\n              lng %4d° %02d' %02d.%02d''\n",
-                lat_1.deg, lat_1.min, lat_1.sec, lat_1.msec, lng_1.deg, lng_1.min, lng_1.sec, lng_1.msec);
-        printf("Вторая точка: lat %4d° %02d' %02d.%02d''\n              lng %4d° %02d' %02d.%02d''\n",
-                lat_2.deg, lat_2.min, lat_2.sec, lat_2.msec, lng_2.deg, lng_2.min, lng_2.sec, lng_2.msec);
+        printf("\nПервая точка: lat %4d° %02d' %f.2''\n              lng %4d° %02d' %.2f''\n",
+                lat_1.deg, lat_1.min, lat_1.sec, lng_1.deg, lng_1.min, lng_1.sec);
+        printf("Вторая точка: lat %4d° %02d' %.2f''\n              lng %4d° %02d' %.2f''\n",
+                lat_2.deg, lat_2.min, lat_2.sec, lng_2.deg, lng_2.min, lng_2.sec);
         printf("\nРасстояние = %.f м\nНачальный азимут = %.1f°\n", result[0], result[1]);
         return 0;
     case 3:
@@ -339,15 +331,15 @@ int main(void)
         case 1:
             printf("\nПреобразование координат из гг мм сс.мс в градусы\n");
             printf("\n   Введи через пробел координаты WGS-84 широты: ");
-            res = scanf("%d %d %d.%d", &lat_1.deg, &lat_1.min, &lat_1.sec, &lat_1.msec);
-            if(input_verif_lat(lat_1.deg, lat_1.min, lat_1.sec, lat_1.msec, res) != 0)
+            res = scanf("%d %d %f", &lat_1.deg, &lat_1.min, &lat_1.sec);
+            if(input_verif_lat(lat_1.deg, lat_1.min, lat_1.sec res) != 0)
                 return 0;
             printf("   Введи через пробел координаты WGS-84 долготы: ");
-            res = scanf("%d %d %d.%d", &lng_1.deg, &lng_1.min, &lng_1.sec, &lng_1.msec);
-            if(input_verif_lng(lng_1.deg, lng_1.min, lng_1.sec, lng_1.msec, res) !=0)
+            res = scanf("%d %d %f", &lng_1.deg, &lng_1.min, &lng_1.sec);
+            if(input_verif_lng(lng_1.deg, lng_1.min, lng_1.sec, res) != 0)
                 return 0;
             double result[2];
-            coord_transfer_wgs84(lat_1.deg, lat_1.min, lat_1.sec, lat_1.msec, result);
+            coord_transfer_wgs84(lat_1.deg, lat_1.min, lat_1.sec, result);
             printf("\n   lat  %.6f°\n   lng  %.6f°\n", result[0], result[1]);
             return 0;
         case 2:
