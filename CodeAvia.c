14578180768384ecd60dd7e199054cf6dff28d06
KuzyaDomovoi <vasilyshtanko@gmail.com>
2023-11-24 15:43:02 +0300

    C

--- CodeAvia.c
+++ CodeAvia.c
@@ -72,21 +72,14 @@ void calcfldist_bear(double lat1, double lng1, double lat2, double lng2, double 
     double sl2 = sin(lat_2.lat);
 
     double delta_lng = lng_2.lng - lng_1.lng;
-    double delta_lng2 = lng_1.lng - lng_2.lng;
     double cdelta_lng = cos(delta_lng);
     double sdelta_lng = sin(delta_lng);
-    double cdelta_lng2 = cos(delta_lng2);
-    double sdelta_lng2 = sin(delta_lng2);
 
     double y = sdelta_lng * cl2;
     double x = cl1 * sl2 - sl1 * cl2 * cdelta_lng;
     double angledeg = atan2(y, x) * DEG;
     double initial_bearing = (int)(angledeg + 360) % 360;
-
-    double y2 = sdelta_lng2 * cl1;
-    double x2 = cl2 * sl1 - sl2 * cl1 * cdelta_lng2;
-    double angledeg2 = atan2(y2, x2) * DEG;
-    double end_bearing = (int)(angledeg2 + 180) % 360;
+    double end_bearing = atan(sdelta_lng * cl1 / (cl1 * sl2 * cdelta_lng - sl1 * cl2)) * DEG;
 
     y = sqrt(pow(cl2 * sdelta_lng, 2) + pow(cl1 * sl2 - sl1 * cl2 * cdelta_lng, 2));
     x = sl1 * sl2 + cl1 * cl2 * cdelta_lng;
