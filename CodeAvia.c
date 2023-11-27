793f982440e4c0c90fa6c2bfb057dcd50786f0b8
KuzyaDomovoi <vasilyshtanko@gmail.com>
2023-11-27 13:45:49 +0300

    C

--- CodeAvia.c
+++ CodeAvia.c
@@ -559,18 +559,18 @@ int main(void)
             printf("\nПервая точка:   lat  %02.f° %02.f' %05.2f''\n                lng %03.f° %02.f' %05.2f''\n", 
                     lat_res1[0], lat_res1[1], lat_res2[0], lng_res1[0], lng_res1[1], lng_res2[0]);
             if(lat_res1_1[0] < 0 && lng_res1_1[0] < 0) {
-                printf("\nВторая точка:   lat  %03.f° %02.f' %05.2f''\n                lng %04.f° %02.f' %05.2f''\n", 
+                printf("Вторая точка:   lat  %03.f° %02.f' %05.2f''\n                lng %04.f° %02.f' %05.2f''\n", 
                     lat_res1_1[0], lat_res1_1[1], lat_res2_2[0], lng_res1_1[0], lng_res1_1[1], lng_res2_2[0]);
             } else
             if(lat_res1_1[0] < 0) {
-                printf("\nВторая точка:   lat  %03.f° %02.f' %05.2f''\n                lng  %03.f° %02.f' %05.2f''\n", 
+                printf("Вторая точка:   lat  %03.f° %02.f' %05.2f''\n                lng  %03.f° %02.f' %05.2f''\n", 
                     lat_res1_1[0], lat_res1_1[1], lat_res2_2[0], lng_res1_1[0], lng_res1_1[1], lng_res2_2[0]);
             } else
             if(lng_res1_1[0] < 0) {
-                printf("\nВторая точка:   lat   %0.fd° %02.f' %05.2f''\n                lng %04.f° %02.f' %05.2f''\n", 
+                printf("Вторая точка:   lat   %0.fd° %02.f' %05.2f''\n                lng %04.f° %02.f' %05.2f''\n", 
                     lat_res1_1[0], lat_res1_1[1], lat_res2_2[0], lng_res1_1[0], lng_res1_1[1], lng_res2_2[0]);
             } else
-            printf("\nВторая точка:   lat  %02.f° %02.f' %05.2f''\n                lng %03.f° %02.f' %05.2f''\n", 
+            printf("Вторая точка:   lat  %02.f° %02.f' %05.2f''\n                lng %03.f° %02.f' %05.2f''\n", 
                     lat_res1_1[0], lat_res1_1[1], lat_res2_2[0], lng_res1_1[0], lng_res1_1[1], lng_res2_2[0]);
             calcfldist_bear(lat_1.lat, lng_1.lng, lat_2.lat, lng_2.lng, result_db);
             printf("\nРасстояние = %.1f м\nНачальный азимут = %.6f°\nКонечный азимут = %.6f°\nНа %.f м 1° изменения азимута\n", 
