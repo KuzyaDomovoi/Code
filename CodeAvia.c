96fbd7387c38ee24d25acd7fd58e9956ae4f3b37
KuzyaDomovoi <vasilyshtanko@gmail.com>
2023-12-07 14:31:40 +0300

    C

--- CodeAvia.c
+++ CodeAvia.c
@@ -672,7 +672,7 @@ int main(void)
                 printf("\nПервая точка: lat  %02.f° %02.f' %05.2f''\n              lng %03.f° %02.f' %05.2f''\n", 
                         lat_res1[0], lat_res1[1], lat_res2[0], lng_res1[0], lng_res1[1], lng_res2[0]);
             if(lat_res1_1[0] < 0 && lng_res1_1[0] < 0) {
-                printf("Вторая точка: lat  %03.f° %02.f' %05.2f''\n              lng %04.f° %02.f' %05.2f''\n", 
+                printf("Вторая точка: lat  %02.f° %02.f' %05.2f''\n              lng %04.f° %02.f' %05.2f''\n", 
                     lat_res1_1[0], lat_res1_1[1], lat_res2_2[0], lng_res1_1[0], lng_res1_1[1], lng_res2_2[0]);
             } else
             if(lat_res1_1[0] < 0) {
