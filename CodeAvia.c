3ec4ba750e59dd885c5a0722bdc7c78eae633e0d
KuzyaDomovoi <142651196+KuzyaDomovoi@users.noreply.github.com>
2024-05-28 10:46:40 +0000

    C

--- CodeAvia.c
+++ CodeAvia.c
@@ -459,8 +459,8 @@ void calc_turn(double aircr_speed, double turn_angle, double turn_roll, double r
     }
     maneuver.turn_rad = pow(maneuver.aircr_speed / 3.6, 2) / (G * tan(maneuver.turn_roll * RAD));
     maneuver.range_turnlead = RAD * maneuver.turn_rad * maneuver.turn_angle;
-    if(maneuver.turn_angle > maneuver.turn_rad) {
-        maneuver.turn_angle = ;
+    if(maneuver.turn_angle > 90) {
+        maneuver.turn_angle = maneuver.turn_rad;
     } else maneuver.lineal_preemption = maneuver.turn_rad * tan(maneuver.turn_angle * RAD/ 2);
     maneuver.turn_time = (2 * M_PI * maneuver.aircr_speed / 3.6) / (G * tan(maneuver.turn_roll * RAD)) * maneuver.turn_angle / 360;
     maneuver.turn_time_m = (maneuver.turn_time / 60) % 60;
